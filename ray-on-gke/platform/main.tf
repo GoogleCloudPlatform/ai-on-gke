eb21d767c76aa5d1aa77f462ae6badd243c44d5e
Yiying Zhang <yiyingzhang@google.com>
2023-09-06 14:02:29 -0700

    set load_config_file = false

--- ray-on-gke/platform/main.tf
+++ ray-on-gke/platform/main.tf
@@ -54,6 +54,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(
       data.google_container_cluster.ml_cluster.master_auth[0].cluster_ca_certificate
     )
+    load_config_file = false
   }
 }
 
