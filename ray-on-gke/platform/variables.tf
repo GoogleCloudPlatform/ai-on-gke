069637aac4e74c258915f75b3388ddbdc6655700
Yiying Zhang <yiyingzhang@google.com>
2023-09-06 17:32:17 -0700

    1

--- ray-on-gke/platform/variables.tf
+++ ray-on-gke/platform/variables.tf
@@ -21,7 +21,7 @@ variable "project_id" {
 variable "region" {
   type        = string
   description = "GCP project region or zone"
-  default     = "us-central1"
+  default     = "us-central1-c"
 }
 
 variable "cluster_name" {
