5a414afffc09b39d8cf5d2563fb757e6da0893a7
Yiying Zhang <yiyingzhang@google.com>
2023-09-12 17:13:17 -0700

    change cloudbuild to run terraform apply

--- ray-on-gke/platform/main.tf
+++ ray-on-gke/platform/main.tf
@@ -44,6 +44,7 @@ provider "kubectl" {
   cluster_ca_certificate = base64decode(
     data.google_container_cluster.ml_cluster.master_auth[0].cluster_ca_certificate
   )
+  load_config_file = false
 }
 
 provider "helm" {
