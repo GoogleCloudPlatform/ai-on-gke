b644f400bf8d51c18450ea5bf6196aa88c98a268
Yiying Zhang <yiyingzhang@google.com>
2023-09-13 20:21:25 -0700

    1

--- ray-on-gke/platform/main.tf
+++ ray-on-gke/platform/main.tf
@@ -44,7 +44,11 @@ provider "kubectl" {
   cluster_ca_certificate = base64decode(
     data.google_container_cluster.ml_cluster.master_auth[0].cluster_ca_certificate
   )
-  # load_config_file = false
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = []
+    command     = "gke-gcloud-auth-plugin"
+  }
 }
 
 provider "helm" {
