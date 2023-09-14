d77e297079585468d7814419c1a457c9bb6660eb
Yiying Zhang <yiyingzhang@google.com>
2023-09-13 18:11:30 -0700

    1

--- ray-on-gke/platform/main.tf
+++ ray-on-gke/platform/main.tf
@@ -44,7 +44,6 @@ provider "kubectl" {
   cluster_ca_certificate = base64decode(
     data.google_container_cluster.ml_cluster.master_auth[0].cluster_ca_certificate
   )
-  load_config_file = false
 }
 
 provider "helm" {
