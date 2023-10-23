62b26d95e1c6f331e9e792c6a36396137c834938
Yiying Zhang <yiyingzhang@google.com>
2023-10-23 13:23:18 -0700

    1

--- jupyter-on-gke/variables.tf
+++ jupyter-on-gke/variables.tf
@@ -15,7 +15,7 @@
 variable "namespace" {
   type        = string
   description = "Kubernetes namespace where resources are deployed"
-  default     = "<your user name>"
+  default     = "yiyingzhang"
 }
 
 variable "create_namespace" {
