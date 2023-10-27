b8e8b09d393d5736614ee778691494e5e5c5894f
Alex Bulankou <alexbu@google.com>
2023-10-27 04:52:31 -0700

    e2e tutorial that uses ray, langchain and builds simple backend and frontend

---
+++ tutorials/e2e-genai-langchain-app/src/frontend/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Path to your source index.html file
+      filename: 'index.html',       // Output file to be saved in the 'dist' directory
+      inject: 'body',               // Where to inject the bundled JS
+    }),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+  },
+};
