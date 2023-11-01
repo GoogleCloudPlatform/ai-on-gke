eb73abcb4aa8a36e9f42ab4bd66df8551abd72d7
Ryan O'Leary <ryanaoleary@google.com>
2023-11-01 02:38:45 +0000

    Working TPU Kuberay Webhook Prototype

---
+++ ray-on-gke/kuberay-tpu-webhook/README.md
@@ -0,0 +1,48 @@
+# TPU Kuberay Webhook Setup Guide
+
+This page contains instructions for how to deploy a mutating admission webhook with Kuberay on TPUs.
+
+### Prerequisites
+
+Preinstall on your computer:
+- Docker
+- Kubectl
+- Terraform
+- Helm
+- Gcloud
+
+### Installing the GKE Platform
+
+1. If needed, git clone https://github.com/GoogleCloudPlatform/ai-on-gke
+
+2. `cd ai-on-gke/gke-platform`
+
+3. Edit `variables.tf` with your GCP settings.
+
+4. Change the region or zone to one where TPUs are available (see [this link](https://cloud.google.com/tpu/docs/regions-zones) for details. For v4 TPUs (the default type), the region should be set to `us-central2` or `us-central2-b`.
+
+5. Set the following flags (note that TPUs are currently only supported on GKE standard):
+
+### Installing the Webhook
+
+1. `make install-cert-manager`
+
+2. `make deploy-cert`
+
+3. If deploying webhook across multiple namespaces: `make install-reflector`
+
+4. `make all`
+
+5. `make docker-build` - edit Makefile with your own Docker image if necessary
+
+6. `make docker-push`
+
+7. `make deploy`
+
+### Injecting TPU Environment Variables
+
+After deploying the webhook, follow the steps in ray-on-gke/TPU_GUIDE to setup Ray on GKE with TPUs. Annotate the RayCluster and desired Ray workergroup specs with the `kuberay-tpu-webhook/inject: enabled` label to inform the Webhook to inject the environment variables. Once the Kuberay cluster is deployed, `kubectl describe` the worker pods to verify the `TPU_WORKER_ID` and `TPU_WORKER_HOSTNAMES` environment variables have been properly set.
+
+### Limitations
+
+The webhook stores unique `TPU_WORKER_ID`s in memory, and thus will fail to initialize the environment variables correctly if the webhook pod dies or restarts before intercepting all pods. Additionally, `TPU_WORKER_ID`s and `TPU_WORKER_HOSTNAMES` are not updated or removed after the initial admission request.
---
+++ ray-on-gke/kuberay-tpu-webhook/main.go
@@ -0,0 +1,203 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/rs/zerolog"
+	ray "github.com/ray-project/kuberay/ray-operator/apis/ray/v1alpha1"
+
+	admissionv1 "k8s.io/api/admission/v1"
+	corev1 "k8s.io/api/core/v1"
+)
+
+// map of ray cluster names to # of workers created in the slice
+var sliceToWorkers map[string]int
+
+// unmarshal raycluster from admission request
+func extractRayCluster(admissionReview *admissionv1.AdmissionReview) (*ray.RayCluster, error) {
+	if admissionReview.Request.Kind.Kind != "RayCluster" {
+		return nil, fmt.Errorf("Expected RayCluster but got %s", admissionReview.Request.Kind.Kind)
+	}
+
+	rayCluster := ray.RayCluster{}
+	if err := json.Unmarshal(admissionReview.Request.Object.Raw, &rayCluster); err != nil {
+		return nil, err
+	}
+
+	return &rayCluster, nil
+}
+
+// add TPU_WORKER_HOSTNAMES to containers in a ray cluster
+func mutateRayCluster(
+	admissionReview *admissionv1.AdmissionReview,
+) (*admissionv1.AdmissionResponse, error) {
+	raycluster, _ := extractRayCluster(admissionReview)
+	patches := []map[string]interface{}{}
+
+	for i := 0; i < len(raycluster.Spec.WorkerGroupSpecs); i++ {
+		numWorkers := int(*raycluster.Spec.WorkerGroupSpecs[i].Replicas)
+
+		// generate hostnames - TODO: make these hostnames DNS addressable with headless-svc
+		serviceName := "headless-svc"
+		hostNames := make([]string, numWorkers)
+		for j := 0; j < numWorkers; j++ {
+			hostNames[j] = fmt.Sprintf("worker-%d.%s", j, serviceName)
+		}
+		joinedHostNames := strings.Join(hostNames, ",")
+
+		// inject hostnames into ray worker pods
+		for j := 0; j < len(raycluster.Spec.WorkerGroupSpecs[i].Template.Spec.Containers); j++ {
+			container := raycluster.Spec.WorkerGroupSpecs[i].Template.Spec.Containers[j]
+			patch := map[string]interface{}{
+				"op": "add",
+			}
+			path := fmt.Sprintf("/spec/workerGroupSpecs/%d/template/spec/containers/%d/env", i, j)
+			value := corev1.EnvVar{
+				Name:  "TPU_WORKER_HOSTNAMES",
+				Value: joinedHostNames,
+			}
+			if len(container.Env) == 0 {
+				patch["path"] = path
+				patch["value"] = []corev1.EnvVar{value}
+			} else {
+				patch["path"] = fmt.Sprintf("%s/-", path)
+				patch["value"] = value
+			}
+			patches = append(patches, patch)
+		}
+	}
+	patchBytes, _ := json.Marshal(patches)
+
+ 	// Create AdmissionResponse
+	admissionResponse := &admissionv1.AdmissionResponse{
+		UID: 	 admissionReview.Request.UID,
+		Allowed: true,
+		Patch:   patchBytes,
+		PatchType: func() *admissionv1.PatchType {
+			pt := admissionv1.PatchTypeJSONPatch
+			return &pt
+		}(),
+	}
+	return admissionResponse, nil
+}
+
+// unmarshal pod from admission request
+func extractPod(admissionReview *admissionv1.AdmissionReview) (*corev1.Pod, error) {
+	if admissionReview.Request.Kind.Kind != "Pod" {
+		return nil, fmt.Errorf("Expected Pod but got %s", admissionReview.Request.Kind.Kind)
+	}
+
+	pod := corev1.Pod{}
+	if err := json.Unmarshal(admissionReview.Request.Object.Raw, &pod); err != nil {
+		return nil, err
+	}
+
+	return &pod, nil
+}
+
+// add TPU_WORKER_ID to pod environment
+func mutatePod(
+	admissionReview *admissionv1.AdmissionReview,
+) (*admissionv1.AdmissionResponse, error) {
+	pod, _ := extractPod(admissionReview)
+
+	// ray operator only sets GenerateName field - doesn't include random suffix until after admission request
+	// have to use mapping of cluster name -> # workers created to set TPU_WORKER_IDs
+	clusterName := pod.Labels["ray.io/cluster"]
+
+	// assign to the next unique ID in the pod slice
+	tpu_worker_id := sliceToWorkers[clusterName]
+	sliceToWorkers[clusterName] += 1
+
+	// create patch to tell pod how to modify environment
+	patches := []map[string]interface{}{}
+
+	// inject the TPU_WORKER_ID environment variable into each container
+	for i := 0; i < len(pod.Spec.Containers); i++ {
+		path := fmt.Sprintf("/spec/containers/%d/env", i)
+		value := corev1.EnvVar{
+			Name:  "TPU_WORKER_ID",
+			Value: fmt.Sprint(tpu_worker_id),
+		}
+		patch := map[string]interface{}{
+			"op": "add",
+		}
+		if(len(pod.Spec.Containers[i].Env) == 0) {
+			patch["path"] = path
+			patch["value"] = []corev1.EnvVar{value}
+		} else {
+			patch["path"] = fmt.Sprintf("%s/-", path)
+			patch["value"] = value
+		}
+		patches = append(patches, patch)
+	}
+
+	patchBytes, _ := json.Marshal(patches)
+
+	admissionResponse := &admissionv1.AdmissionResponse{
+		UID: 	 admissionReview.Request.UID,
+		Allowed: true,
+		Patch:   patchBytes,
+		PatchType: func() *admissionv1.PatchType {
+			pt := admissionv1.PatchTypeJSONPatch
+			return &pt
+		}(),
+	}
+	return admissionResponse, nil
+}
+
+func init() {
+	sliceToWorkers = make(map[string]int)
+}
+
+func main() {
+	cert := "/etc/kuberay-tpu-webhook/tls/tls.crt"
+	key := "/etc/kuberay-tpu-webhook/tls/tls.key"
+	log := zerolog.New(os.Stdout).With().Timestamp().Logger()
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "kuberay-tpu-webhook")
+	})
+	mux.HandleFunc("/inject", func(w http.ResponseWriter, r *http.Request) {
+		admissionReview := &admissionv1.AdmissionReview{}
+		if err := json.NewDecoder(r.Body).Decode(admissionReview); err != nil {
+			http.Error(w, "Error decoding request body", http.StatusBadRequest)
+			return
+		}
+
+		if admissionReview.Request.Kind.Kind == "RayCluster" {
+			log.Debug().Msg("Received review for RayCluster")
+			admissionReview.Response, _ = mutateRayCluster(admissionReview)
+			responseBytes, _ := json.Marshal(admissionReview)
+			fmt.Fprint(w, string(responseBytes))
+			return
+		}
+
+		if admissionReview.Request.Kind.Kind == "Pod" {
+			log.Debug().Msg("Received review for Pod")
+			admissionReview.Response, _ = mutatePod(admissionReview)
+			responseBytes, _ := json.Marshal(admissionReview)
+			fmt.Fprint(w, string(responseBytes))
+			return
+		}
+	})
+
+	srv := &http.Server{
+		Addr:    ":443",
+		Handler: mux,
+	}
+
+	if err := srv.ListenAndServeTLS(cert, key); err != nil {
+		if err == http.ErrServerClosed {
+			log.Info().Msg("Server closed")
+			return
+		}
+		log.Fatal().Err(err).Msg("Failed to start server")
+	}
+}
