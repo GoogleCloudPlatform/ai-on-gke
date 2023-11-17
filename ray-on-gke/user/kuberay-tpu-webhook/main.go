81bf7966319933cc417332904bbfc3fbe051ad45
Ryan O'Leary <ryanaoleary@google.com>
2023-11-17 00:09:38 +0000

    Changed logging and fixed TPU checks

--- ray-on-gke/user/kuberay-tpu-webhook/main.go
+++ ray-on-gke/user/kuberay-tpu-webhook/main.go
@@ -4,18 +4,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
 	"strings"
 
-	"github.com/rs/zerolog"
 	ray "github.com/ray-project/kuberay/ray-operator/apis/ray/v1"
-
 	admissionv1 "k8s.io/api/admission/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/klog/v2"
 )
 
 // our representation of a pod slice
-// not necessarily true that worker groups scheduled on 1 slice
+// not necessarily true that worker group scheduled on 1 slice
 type Slice struct {
 	rayClusterName	string
 	groupName	string
@@ -23,6 +21,8 @@ type Slice struct {
 
 var (
 	tpuResourceName = corev1.ResourceName("google.com/tpu")
+	certPath = "/etc/kuberay-tpu-webhook/tls/tls.crt"
+	keyPath = "/etc/kuberay-tpu-webhook/tls/tls.key"
 
 	// map of ray cluster names to # of workers created in the slice
 	sliceToWorkers map[Slice]int
@@ -67,37 +67,39 @@ func mutateRayCluster(
 	patches := []map[string]interface{}{}
 
 	for i := 0; i < len(raycluster.Spec.WorkerGroupSpecs); i++ {
-		numWorkers := int(*raycluster.Spec.WorkerGroupSpecs[i].Replicas)
+		if(containerRequestingTPUs(raycluster.Spec.WorkerGroupSpecs[i].Template.Spec.Containers...)) {
+			numWorkers := int(*raycluster.Spec.WorkerGroupSpecs[i].Replicas)
 
-		// generate hostnames - TODO: make these hostnames DNS addressable with headless-svc
-		workerGroupName := raycluster.Spec.WorkerGroupSpecs[i].GroupName
-		serviceName := "headless-svc"
-		hostNames := make([]string, numWorkers)
-		for j := 0; j < numWorkers; j++ {
-			hostNames[j] = fmt.Sprintf("%s-%d.%s", workerGroupName, j, serviceName)
-		}
-		joinedHostNames := strings.Join(hostNames, ",")
+			// generate DNS hostnames
+			workerGroupName := raycluster.Spec.WorkerGroupSpecs[i].GroupName
+			serviceName := "headless-svc" // TODO: get headless-svc associated with workergroup
+			hostNames := make([]string, numWorkers)
+			for j := 0; j < numWorkers; j++ {
+				hostNames[j] = fmt.Sprintf("%s-%d.%s", workerGroupName, j, serviceName)
+			}
+			joinedHostNames := strings.Join(hostNames, ",")
 
-		// inject hostnames into ray worker pods
-		for j := 0; j < len(raycluster.Spec.WorkerGroupSpecs[i].Template.Spec.Containers); j++ {
-			container := raycluster.Spec.WorkerGroupSpecs[i].Template.Spec.Containers[j]
-			if(containerRequestingTPUs(container)) {
-				patch := map[string]interface{}{
-					"op": "add",
+			// inject hostnames into ray worker pods
+			for j := 0; j < len(raycluster.Spec.WorkerGroupSpecs[i].Template.Spec.Containers); j++ {
+				container := raycluster.Spec.WorkerGroupSpecs[i].Template.Spec.Containers[j]
+				if(containerRequestingTPUs(container)) {
+					patch := map[string]interface{}{
+						"op": "add",
+					}
+					path := fmt.Sprintf("/spec/workerGroupSpecs/%d/template/spec/containers/%d/env", i, j)
+					value := corev1.EnvVar{
+						Name:  "TPU_WORKER_HOSTNAMES",
+						Value: joinedHostNames,
+					}
+					if len(container.Env) == 0 {
+						patch["path"] = path
+						patch["value"] = []corev1.EnvVar{value}
+					} else {
+						patch["path"] = fmt.Sprintf("%s/-", path)
+						patch["value"] = value
+					}
+					patches = append(patches, patch)
 				}
-				path := fmt.Sprintf("/spec/workerGroupSpecs/%d/template/spec/containers/%d/env", i, j)
-				value := corev1.EnvVar{
-					Name:  "TPU_WORKER_HOSTNAMES",
-					Value: joinedHostNames,
-				}
-				if len(container.Env) == 0 {
-					patch["path"] = path
-					patch["value"] = []corev1.EnvVar{value}
-				} else {
-					patch["path"] = fmt.Sprintf("%s/-", path)
-					patch["value"] = value
-				}
-				patches = append(patches, patch)
 			}
 		}
 	}
@@ -142,45 +144,38 @@ func mutatePod(
 	groupName := pod.Labels["ray.io/group"]
 	podSlice := Slice{clusterName, groupName}
 
-	// assign to the next unique ID in the pod slice
-	tpu_worker_id := sliceToWorkers[podSlice]
-	sliceToWorkers[podSlice] += 1
-
 	// create patch to tell pod how to modify environment
 	patches := []map[string]interface{}{}
 
-	// inject the TPU_WORKER_ID environment variable into the container reguesting TPUs
-	for i := 0; i < len(pod.Spec.Containers); i++ {
-		container := pod.Spec.Containers[i]
-		if(containerRequestingTPUs(container)) {
-			path := fmt.Sprintf("/spec/containers/%d/env", i)
-			value1 := corev1.EnvVar{
-				Name:  "TPU_WORKER_ID",
-				Value: fmt.Sprint(tpu_worker_id),
+	// inject the TPU_WORKER_ID environment variable into the container requesting TPUs
+	if(containerRequestingTPUs(pod.Spec.Containers...)) {
+		// assign to the next unique ID in the pod slice
+		tpu_worker_id := sliceToWorkers[podSlice]
+		sliceToWorkers[podSlice] += 1
+		for i := 0; i < len(pod.Spec.Containers); i++ {
+			container := pod.Spec.Containers[i]
+			if(containerRequestingTPUs(container)) {
+				path := fmt.Sprintf("/spec/containers/%d/env", i)
+				value1 := corev1.EnvVar{
+					Name:  "TPU_WORKER_ID",
+					Value: fmt.Sprint(tpu_worker_id),
+				}
+				value2 := corev1.EnvVar{
+					Name:  "TPU_NAME",
+					Value: fmt.Sprint(groupName),
+				}
+				patch1, patch2 := map[string]interface{}{"op": "add",}, map[string]interface{}{"op": "add",}
+				if(len(container.Env) == 0) {
+					patch1["path"], patch2["path"] = path, path
+					patch1["value"] = []corev1.EnvVar{value1}
+					patch2["value"] = []corev1.EnvVar{value2}
+				} else {
+					patch1["path"], patch2["path"] = fmt.Sprintf("%s/-", path), fmt.Sprintf("%s/-", path)
+					patch1["value"] = value1
+					patch2["value"] = value2
+				}
+				patches = append(patches, patch1, patch2)
 			}
-			value2 := corev1.EnvVar{
-				Name:  "TPU_NAME",
-				Value: fmt.Sprint(groupName),
-			}
-			patch1 := map[string]interface{}{
-				"op": "add",
-			}
-			patch2 := map[string]interface{}{
-				"op": "add",
-			}
-			if(len(container.Env) == 0) {
-				patch1["path"] = path
-				patch1["value"] = []corev1.EnvVar{value1}
-				patch2["path"] = path
-				patch2["value"] = []corev1.EnvVar{value2}
-			} else {
-				patch1["path"] = fmt.Sprintf("%s/-", path)
-				patch1["value"] = value1
-				patch2["path"] = fmt.Sprintf("%s/-", path)
-				patch2["value"] = value2
-			}
-			patches = append(patches, patch1)
-			patches = append(patches, patch2)
 		}
 	}
 
@@ -203,10 +198,6 @@ func init() {
 }
 
 func main() {
-	cert := "/etc/kuberay-tpu-webhook/tls/tls.crt"
-	key := "/etc/kuberay-tpu-webhook/tls/tls.key"
-	log := zerolog.New(os.Stdout).With().Timestamp().Logger()
-
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -220,7 +211,7 @@ func main() {
 		}
 
 		if admissionReview.Request.Kind.Kind == "RayCluster" {
-			log.Debug().Msg("Received review for RayCluster")
+			klog.Info("Received review for RayCluster")
 			admissionReview.Response, _ = mutateRayCluster(admissionReview)
 			responseBytes, _ := json.Marshal(admissionReview)
 			fmt.Fprint(w, string(responseBytes))
@@ -228,7 +219,7 @@ func main() {
 		}
 
 		if admissionReview.Request.Kind.Kind == "Pod" {
-			log.Debug().Msg("Received review for Pod")
+			klog.Info("Received review for Pod")
 			admissionReview.Response, _ = mutatePod(admissionReview)
 			responseBytes, _ := json.Marshal(admissionReview)
 			fmt.Fprint(w, string(responseBytes))
@@ -241,11 +232,11 @@ func main() {
 		Handler: mux,
 	}
 
-	if err := srv.ListenAndServeTLS(cert, key); err != nil {
+	if err := srv.ListenAndServeTLS(certPath, keyPath); err != nil {
 		if err == http.ErrServerClosed {
-			log.Info().Msg("Server closed")
+			klog.Info("Server closed")
 			return
 		}
-		log.Fatal().Err(err).Msg("Failed to start server")
+		klog.Error("Failed to start server")
 	}
 }
