288854c37fe6eb47d57eb06101470038073fd966
Nick Stogner <nicholas.stogner@gmail.com>
2024-02-21 12:21:16 -0500

    Simplify scopes

--- gke-disk-image-builder/imager.go
+++ gke-disk-image-builder/imager.go
@@ -154,13 +154,7 @@ func GenerateDiskImage(ctx context.Context, req Request) error {
 								&compute.ServiceAccount{
 									Email: req.ServiceAccount,
 									Scopes: []string{
-										"https://www.googleapis.com/auth/devstorage.read_only",
-										"https://www.googleapis.com/auth/logging.write",
-										"https://www.googleapis.com/auth/monitoring.write",
-										"https://www.googleapis.com/auth/pubsub",
-										"https://www.googleapis.com/auth/service.management.readonly",
-										"https://www.googleapis.com/auth/servicecontrol",
-										"https://www.googleapis.com/auth/trace.append",
+										"https://www.googleapis.com/auth/cloud-platform",
 									},
 								},
 							},
