GoogleCloudPlatform/ai-on-gke

commit 4b9460fc9cb06645ffee06ef0a5c63b97f6dc2d8

Optimize CI (#303)
* upgrade machineType

* pinned workload-identity module to fix for sa bug

* Modified all occurances of WI module

* remove automount_service_account_token from kuberay-operator

modules/kuberay-operator/kuberay.tf
@@ -25,13 +25,12 @@ resource "helm_release" "kuberay-operator" {
 
 module "kuberay-workload-identity" {
   source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  version             = "30.0.0" # Pinning to a previous version as current version (30.1.0) showed inconsitent behaviour with workload identity service accounts
   use_existing_gcp_sa = !var.create_service_account
   name                = var.google_service_account
   namespace           = var.namespace
   project_id          = var.project_id
   roles               = ["roles/cloudsql.client", "roles/monitoring.viewer"]
-
-  automount_service_account_token = true
 
   depends_on = [helm_release.kuberay-operator]
 }

applications/rag/frontend/main.tf
@@ -63,6 +63,7 @@ module "iap_auth" {
 
 module "frontend-workload-identity" {
   source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  version             = "30.0.0" # Pinning to a previous version as current version (30.1.0) showed inconsitent behaviour with workload identity service accounts
   use_existing_gcp_sa = !var.create_service_account
   name                = var.google_service_account
   namespace           = var.namespace

cloudbuild.yaml
@@ -363,4 +363,4 @@ substitutions:
   _USER_NAME: github
 options:
   substitutionOption: 'ALLOW_LOOSE'
-
+  machineType: 'E2_HIGHCPU_8'

modules/jupyter/main.tf
@@ -67,6 +67,7 @@ module "iap_auth" {
 
 module "jupyterhub-workload-identity" {
   source     = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  version    = "30.0.0" # Pinning to a previous version as current version (30.1.0) showed inconsitent behaviour with workload identity service accounts
   name       = var.workload_identity_service_account
   namespace  = var.namespace
   project_id = var.project_id

commit 836bd71fef6b4ddf8d3987d014d3553f5909e6cc

Fix regression for kuberay-operator token mounting
Introduced in https://github.com/GoogleCloudPlatform/ai-on-gke/pull/303

modules/kuberay-operator/kuberay.tf
@@ -31,7 +31,10 @@ module "kuberay-workload-identity" {
   namespace           = var.namespace
   project_id          = var.project_id
   roles               = ["roles/cloudsql.client", "roles/monitoring.viewer"]
-  depends_on          = [helm_release.kuberay-operator]
+
+  automount_service_account_token = true
+
+  depends_on = [helm_release.kuberay-operator]
 }
 
 resource "kubernetes_secret_v1" "service_account_token" {