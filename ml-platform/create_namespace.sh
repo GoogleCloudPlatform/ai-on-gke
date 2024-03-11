249c09dac362773e6a8a865e2a5042d9e250f3aa
gushob21 <gushob@google.com>
2024-03-11 17:01:54 +0000

    Adding default single env installation and updating READMEs

---
+++ ml-platform/create_namespace.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+configsync_repo_name=${1}
+github_email=${2}
+github_org=${3}
+github_user=${4}
+namespace=${5}
+
+logfile=$(pwd)/log
+random=$(echo $RANDOM | md5sum | head -c 20; echo)
+download_acm_repo_name="/tmp/$(echo ${configsync_repo_name} | awk -F "/" '{print $2}')-${random}"
+git config --global user.name ${github_user}
+git config --global user.email ${github_emai}
+git clone https://${github_user}:${TF_VAR_github_token}@github.com/${configsync_repo_name} ${download_acm_repo_name}
+cd ${download_acm_repo_name}/manifests/clusters
+
+if [ -d "${namespace}" ]; then
+  exit 0
+fi
+chars_in_namespace=$(echo -n ${namespace} | wc -c)
+#adding 4 for number of chars in "dev-"
+chars_in_reposync_name=$(expr $chars_in_namespace + 4)
+mkdir ${namespace} || exit 1
+cp -r ../../templates/_cluster_template/team/* ${namespace}
+sed -i "s?NAMESPACE?$namespace?g" ${namespace}/*
+sed -ni '/#END OF SINGLE ENV DECLARATION/q;p' ${namespace}/reposync.yaml
+sed -i "s?GIT_REPO?https://github.com/$configsync_repo_name?g" ${namespace}/reposync.yaml
+sed -i "s?<NUMBER_OF_CHARACTERS_IN_REPOSYNC_NAME>?$chars_in_reposync_name?g" ${namespace}/reposync.yaml
+
+mkdir ../apps/${namespace}
+touch ../apps/${namespace}/.gitkeep
+
+cat <<EOF >>kustomization.yaml
+- ./${namespace}
+EOF
+cd ..
+git add .
+git config --global user.name ${github_user}
+git config --global user.email ${github_email}
+git commit -m "Adding manifests to create a new namespace."
+git push origin
+
+cd -
+rm -rf ${download_acm_repo_name}
---
+++ ml-platform/modules/node-pools/nodepools.tf
@@ -0,0 +1,71 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+resource "google_container_node_pool" "node-pool" {
+  name     = format("%s-%s", var.cluster_name, var.node_pool_name)
+  project  = var.project_id
+  cluster  = var.cluster_name
+  location = var.region
+  node_config {
+    machine_type = var.machine_type
+    dynamic "taint" {
+      for_each = var.taints
+      content {
+        key    = taint.value.key
+        value  = taint.value.value
+        effect = taint.value.effect
+      }
+    }
+    labels = {
+      "resource-type" : var.resource_type
+    }
+
+    guest_accelerator {
+      type  = var.accelerator
+      count = var.accelerator_count
+    }
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+
+    dynamic "reservation_affinity" {
+      for_each = var.reservation_name != "" ? [1] : []
+      content {
+        consume_reservation_type = "SPECIFIC_RESERVATION"
+        key                      = "compute.googleapis.com/reservation-name"
+        values                   = [var.reservation_name]
+      }
+    }
+  }
+  autoscaling {
+    total_min_node_count = var.autoscaling["total_min_node_count"]
+    total_max_node_count = var.autoscaling["total_max_node_count"]
+    location_policy      = var.autoscaling["location_policy"]
+  }
+
+  timeouts {
+    create = "30m"
+    update = "20m"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      node_config[0].labels,
+      node_config[0].taint,
+    ]
+  }
+  network_config {
+    enable_private_nodes = true
+  }
+}
