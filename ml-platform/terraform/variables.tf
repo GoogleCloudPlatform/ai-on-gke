40d1e38f1407f77537ec7ea4e3f80ec2d06f78c9
arueth <arueth@gmail.com>
2024-03-14 16:09:19 +0000

    Alphabetized and standardized variables.tf

--- ml-platform/terraform/variables.tf
+++ ml-platform/terraform/variables.tf
@@ -12,169 +12,57 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-variable "org_id" {
-  type        = string
-  description = "The GCP orig id"
-  default     = null
-}
-
-variable "env" {
-  type        = set(string)
-  description = "List of environments"
-  default     = ["dev"]
-}
-
-variable "default_env" {
-  type        = string
-  description = "Lowest environments"
-  default     = "dev"
-}
-
-variable "folder_id" {
-  type        = string
-  description = "Folder Id where the GCP projects will be created"
-  default     = null
-}
-
 variable "billing_account" {
-  type        = string
+  default     = null
   description = "GCP billing account"
-  default     = null
-}
-
-variable "project_name" {
-  type        = string
-  description = "GCP project name"
-  default     = null
-}
-
-variable "create_projects" {
-  type        = number
-  description = "Flag to create GCP projects"
-  default     = 0
-}
-
-variable "project_id" {
-  type        = map(any)
-  description = "The GCP project where the resources will be created. It is a map with environments as keys and project_ids s values"
-}
-
-variable "network_name" {
-  default     = "ml-vpc"
-  description = "VPC network where GKE cluster will be created"
-  type        = string
-}
-
-variable "routing_mode" {
-  default     = "GLOBAL"
-  description = "VPC routing mode."
-  type        = string
-}
-
-variable "subnet_01_name" {
-  default     = "ml-vpc-subnet-01"
-  description = "Name of the first subnet in the VPC network."
-  type        = string
-}
-
-variable "subnet_01_ip" {
-  default     = "10.40.0.0/22"
-  description = "CIDR of the first subnet."
-  type        = string
-}
-
-variable "subnet_01_region" {
-  default     = "us-central1"
-  description = "Region of the first subnet."
-  type        = string
-}
-
-variable "subnet_01_description" {
-  default     = "subnet 01"
-  description = "Description of the first subnet."
-  type        = string
-}
-
-variable "subnet_02_name" {
-  default     = "gke-vpc-subnet-02"
-  description = "Name of the second subnet in the VPC network."
-  type        = string
-}
-
-variable "subnet_02_ip" {
-  default     = "10.12.0.0/22"
-  description = "CIDR of the second subnet."
-  type        = string
-}
-
-variable "subnet_02_region" {
-  default     = "us-west2"
-  description = "Region of the second subnet."
-  type        = string
-}
-
-variable "subnet_02_description" {
-  default     = "subnet 02"
-  description = "Description of the second subnet."
   type        = string
 }
 
 variable "cluster_name" {
-  description = "Name of the GKE cluster"
   default     = "gke-ml"
+  description = "Name of the GKE cluster"
   type        = string
 }
 
-variable "reserved_taints" {
-  description = "Taints to be applied to the reserved node pool."
-  type = list(object({
-    key    = string
-    value  = any
-    effect = string
-  }))
-  default = [{
-    key    = "reserved"
-    value  = true
-    effect = "NO_SCHEDULE"
-  }]
-}
-
-variable "ondemand_taints" {
-  description = "Taints to be applied to the on-demand node pool."
-  type = list(object({
-    key    = string
-    value  = any
-    effect = string
-  }))
-  default = [{
-    key    = "ondemand"
-    value  = true
-    effect = "NO_SCHEDULE"
-  }]
-}
-
-variable "spot_taints" {
-  description = "Taints to be applied to the spot node pool."
-  type = list(object({
-    key    = string
-    value  = any
-    effect = string
-  }))
-  default = [{
-    key    = "spot"
-    value  = true
-    effect = "NO_SCHEDULE"
-  }]
+variable "config_management_version" {
+  default     = "1.17.1"
+  description = "Version of Config Management to enable"
+  type        = string
 }
 
 variable "configsync_repo_name" {
-  type        = string
-  description = "Name of the GitHub repo that will be synced to the cluster with Config sync."
   default     = "config-sync-repo"
+  description = "Name of the GitHub repo that will be synced to the cluster with Config sync."
+  type        = string
 }
 
-variable "github_user" {
-  description = "GitHub user name."
+variable "create_namespace" {
+  description = "Setup a namespace to demo."
+  default     = 1
+  type        = number
+}
+
+variable "create_projects" {
+  default     = 0
+  description = "Flag to create GCP projects"
+  type        = number
+}
+
+variable "default_env" {
+  default     = "dev"
+  description = "Lowest environments"
+  type        = string
+}
+
+variable "env" {
+  default     = ["dev"]
+  description = "List of environments"
+  type        = set(string)
+}
+
+variable "folder_id" {
+  default     = null
+  description = "Folder Id where the GCP projects will be created"
   type        = string
 }
 
@@ -184,47 +72,160 @@ variable "github_email" {
 }
 
 variable "github_org" {
-  type        = string
   description = "GitHub org."
+  type        = string
 }
 
 variable "github_token" {
-  type        = string
   description = "GitHub token. It is a token with write permissions as it will create a repo in the GitHub org."
-}
-
-variable "secret_for_rootsync" {
-  type        = number
-  description = "Create git-cred in config-management-system namespace."
-  default     = 1
-}
-
-variable "create_namespace" {
-  type        = number
-  description = "Setup a namespace to demo."
-  default     = 1
-}
-
-variable "namespace" {
   type        = string
-  description = "Name of the namespace to demo."
-  default     = "ml-team"
+}
+
+variable "github_user" {
+  description = "GitHub user name."
+  type        = string
 }
 
 variable "install_kuberay" {
-  type        = number
-  description = "Flag to install kuberay operator."
   default     = 1
+  description = "Flag to install kuberay operator."
+  type        = number
 }
 
 variable "install_ray_in_ns" {
-  type        = number
-  description = "Flag to install ray cluster in the namespace created with the demo."
   default     = 1
+  description = "Flag to install ray cluster in the namespace created with the demo."
+  type        = number
 }
 
-variable "config_management_version" {
+variable "namespace" {
+  default     = "ml-team"
+  description = "Name of the namespace to demo."
   type        = string
-  description = "Version of Config Management to enable"
-  default     = "1.17.1"
+}
+
+variable "network_name" {
+  default     = "ml-vpc"
+  description = "VPC network where GKE cluster will be created"
+  type        = string
+}
+
+variable "ondemand_taints" {
+  default = [{
+    key    = "ondemand"
+    value  = true
+    effect = "NO_SCHEDULE"
+  }]
+  description = "Taints to be applied to the on-demand node pool."
+  type = list(object({
+    key    = string
+    value  = any
+    effect = string
+  }))
+}
+
+variable "org_id" {
+  default     = null
+  description = "The GCP orig id"
+  type        = string
+}
+
+variable "project_id" {
+  description = "The GCP project where the resources will be created. It is a map with environments as keys and project_ids s values"
+  type        = map(any)
+}
+
+variable "project_name" {
+  default     = null
+  description = "GCP project name"
+  type        = string
+}
+
+variable "reserved_taints" {
+  default = [{
+    key    = "reserved"
+    value  = true
+    effect = "NO_SCHEDULE"
+  }]
+  description = "Taints to be applied to the reserved node pool."
+  type = list(object({
+    key    = string
+    value  = any
+    effect = string
+  }))
+}
+
+variable "routing_mode" {
+  default     = "GLOBAL"
+  description = "VPC routing mode."
+  type        = string
+}
+
+variable "secret_for_rootsync" {
+  default     = 1
+  description = "Create git-cred in config-management-system namespace."
+  type        = number
+
+}
+
+variable "spot_taints" {
+  default = [{
+    key    = "spot"
+    value  = true
+    effect = "NO_SCHEDULE"
+  }]
+  description = "Taints to be applied to the spot node pool."
+  type = list(object({
+    key    = string
+    value  = any
+    effect = string
+  }))
+}
+
+variable "subnet_01_description" {
+  default     = "subnet 01"
+  description = "Description of the first subnet."
+  type        = string
+}
+
+variable "subnet_01_ip" {
+  default     = "10.40.0.0/22"
+  description = "CIDR of the first subnet."
+  type        = string
+}
+
+variable "subnet_01_name" {
+  default     = "ml-vpc-subnet-01"
+  description = "Name of the first subnet in the VPC network."
+  type        = string
+}
+
+variable "subnet_01_region" {
+  default     = "us-central1"
+  description = "Region of the first subnet."
+  type        = string
+}
+
+variable "subnet_02_description" {
+  default     = "subnet 02"
+  description = "Description of the second subnet."
+  type        = string
+}
+
+variable "subnet_02_ip" {
+  default     = "10.12.0.0/22"
+  description = "CIDR of the second subnet."
+  type        = string
+}
+
+variable "subnet_02_name" {
+  default     = "gke-vpc-subnet-02"
+  description = "Name of the second subnet in the VPC network."
+  type        = string
+}
+
+variable "subnet_02_region" {
+  default     = "us-west2"
+  description = "Region of the second subnet."
+  type        = string
 }
