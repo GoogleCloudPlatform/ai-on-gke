c25b7f3972cefb2dd79bce03cd0f13ad1f9ae004
Nick Stogner <nstogner@users.noreply.github.com>
2024-03-19 18:42:35 -0400

    TPU Provisioner: Fix skipped scale ups & garbage collect zombie node pools (#277)
    * Switch to using owner UID for node pool naming and retry on creation of node pool in stopping state
    
    * Rename error
    
    * Fix not-found handling on delete and fix event msg
    
    * Add node pool garbage collector and improve events
    
    * Update garbage collector logging and fix case where pod ref does not exist
    
    * Add logs
    
    * Make secure-boot configurable
    
    * Fix typo in log
    
    * Fix integration tests
    
    * Address comments and make node pool deletion delay configurable to allow tests to pass

--- tpu-provisioner/internal/controller/provider_test.go
+++ tpu-provisioner/internal/controller/provider_test.go
@@ -4,19 +4,24 @@ import (
 	"sync"
 	"time"
 
+	"github.com/GoogleCloudPlatform/ai-on-gke/tpu-provisioner/internal/cloud"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
+
+var _ cloud.Provider = &testProvider{}
 
 type testProvider struct {
 	sync.Mutex
 	created map[types.NamespacedName]bool
 	deleted map[string]time.Time
+
+	cloud.Provider
 }
 
 func (p *testProvider) NodePoolLabelKey() string { return "cloud.test.com/test-nodepool" }
 
-func (p *testProvider) EnsureNodePoolForPod(pod *corev1.Pod) error {
+func (p *testProvider) EnsureNodePoolForPod(pod *corev1.Pod, _ string) error {
 	p.Lock()
 	defer p.Unlock()
 	p.created[types.NamespacedName{Namespace: pod.Namespace, Name: pod.Name}] = true
@@ -29,7 +34,7 @@ func (p *testProvider) getCreated(nn types.NamespacedName) bool {
 	return p.created[nn]
 }
 
-func (p *testProvider) DeleteNodePoolForNode(node *corev1.Node) error {
+func (p *testProvider) DeleteNodePoolForNode(node *corev1.Node, _ string) error {
 	p.Lock()
 	defer p.Unlock()
 	if _, exists := p.deleted[node.Name]; !exists {
