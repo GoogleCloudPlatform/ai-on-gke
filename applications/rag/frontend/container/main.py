0f2799c3c89a7beaaac3ef5f9a4313fbacf12c4c
yiyinglovecoding <113492735+yiyinglovecoding@users.noreply.github.com>
2024-03-22 19:31:02 -0700

    Catch Frontend DatabaseError (#432)
    * catch DatabaseError
    
    * typo
    
    * catch all other errors

--- applications/rag/frontend/container/main.py
+++ applications/rag/frontend/container/main.py
@@ -140,20 +140,31 @@ def index():
 
 def fetchContext(query_text):
     with db.connect() as conn:
-        results = conn.execute(sqlalchemy.text("SELECT * FROM " + TABLE_NAME)).fetchall()
-        log.info(f"query database results:")
-        for row in results:
-            print(row)
+        try: 
+            results = conn.execute(sqlalchemy.text("SELECT * FROM " + TABLE_NAME)).fetchall()
+            log.info(f"query database results:")
+            for row in results:
+                print(row)
 
-        # chunkify query & fetch matches
-        query_emb = transformer.encode(query_text).tolist()
-        query_request = "SELECT id, text, text_embedding, 1 - ('[" + ",".join(map(str, query_emb)) + "]' <=> text_embedding) AS cosine_similarity FROM " + TABLE_NAME + " ORDER BY cosine_similarity DESC LIMIT 5;"
-        query_results = conn.execute(sqlalchemy.text(query_request)).fetchall()
-        conn.commit()
-        log.info(f"printing matches:")
-        for row in query_results:
-            print(row)
+            # chunkify query & fetch matches
+            query_emb = transformer.encode(query_text).tolist()
+            query_request = "SELECT id, text, text_embedding, 1 - ('[" + ",".join(map(str, query_emb)) + "]' <=> text_embedding) AS cosine_similarity FROM " + TABLE_NAME + " ORDER BY cosine_similarity DESC LIMIT 5;"
+            query_results = conn.execute(sqlalchemy.text(query_request)).fetchall()
+            conn.commit()
+            log.info(f"printing matches:")
+            for row in query_results:
+                print(row)
+        except sqlalchemy.exc.DatabaseError as err: 
+            log.info(f"Table {TABLE_NAME} does not exist: {err}")
+            return ""
+        except Exception as err:
+            log.info(f"{err}")
+            return ""
 
+    if query_results.all()==None:
+        log.info(f"Table {TABLE_NAME} return empty result")
+        return ""
+    
     return query_results[0][1]
 
 @app.route('/prompt', methods=['POST'])
