138d1a2a90df659fa48448ad33d40c2921d65a10
umeshkumhar <umeshkumhar@google.com>
2024-03-27 02:02:52 +0530

    fix namespace and network auto creation

--- applications/rag/frontend/main.tf
+++ applications/rag/frontend/main.tf
@@ -26,7 +26,7 @@ locals {
 # IAP Section: Creates the GKE components
 module "iap_auth" {
   count  = var.add_auth ? 1 : 0
-  source = "../../modules/iap"
+  source = "../../../modules/iap"
 
   project_id               = var.project_id
   namespace                = var.namespace
