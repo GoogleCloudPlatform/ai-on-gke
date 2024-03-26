d4d7baaca301fe96ddd064a271f52fe769ff6029
Ashok Chandrasekar <achandrasekar@google.com>
2024-03-26 20:08:31 +0000

    Add vLLM model server support for inference benchmarks
    This change adds vllm as a supported model server for benchmarking. The
    change includes:
    
    1. vLLM automation through Terraform to different models with various
       configurations.
    2. vLLM as a backend in the benchmarking tool.
    3. vLLM request/response parsing to accurately measure throughput tokens
       and latency.

---
+++ benchmarks/inference-server/vllm/variables.tf
@@ -0,0 +1,97 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "credentials_config" {
+  description = "Configure how Terraform authenticates to the cluster."
+  type = object({
+    fleet_host = optional(string)
+    kubeconfig = optional(object({
+      context = optional(string)
+      path    = optional(string, "~/.kube/config")
+    }))
+  })
+  nullable = false
+  validation {
+    condition = (
+      (var.credentials_config.fleet_host != null) !=
+      (var.credentials_config.kubeconfig != null)
+    )
+    error_message = "Exactly one of fleet host or kubeconfig must be set."
+  }
+}
+
+variable "namespace" {
+  description = "Namespace used for vLLM resources."
+  type        = string
+  nullable    = false
+  default     = "default"
+}
+
+variable "model_id" {
+  description = "Model used for inference."
+  type        = string
+  nullable    = false
+  default     = "tiiuae/falcon-7b"
+}
+
+variable "gpu_count" {
+  description = "Parallelism based on number of gpus."
+  type        = number
+  nullable    = false
+  default     = 1
+  validation {
+    condition     = var.gpu_count == 1 || var.gpu_count == 2 || var.gpu_count == 4 || var.gpu_count == 8
+    error_message = "vLLM server supports gpu_count equal to one of 1, 2, 4, or 8."
+  }
+}
+
+variable "ksa" {
+  description = "Kubernetes Service Account used for workload."
+  type        = string
+  nullable    = false
+  default     = "default"
+}
+
+variable "templates_path" {
+  description = "Path where manifest templates will be read from. Set to null to use the default manifests"
+  type        = string
+  default     = null
+}
+
+variable "secret_templates_path" {
+  description = "Path where secret configuration manifest templates will be read from. Set to null to use the default manifests"
+  type        = string
+  default     = null
+}
+
+variable "hugging_face_secret" {
+  description = "Secret id in Secret Manager"
+  type        = string
+  nullable    = true
+  default     = null
+}
+
+variable "hugging_face_secret_version" {
+  description = "Secret version in Secret Manager"
+  type        = string
+  nullable    = true
+  default     = null
+}
+
+variable "project_id" {
+  description = "Project id of existing or created project."
+  type        = string
+}
