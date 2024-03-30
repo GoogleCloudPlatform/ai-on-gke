8961bb3594e15409f16ad0209cfc6d3304b0672f
Richard Liu <ricliu@google.com>
2024-03-30 00:50:21 +0000

    terraform fmt

--- applications/rag/main.tf
+++ applications/rag/main.tf
@@ -192,8 +192,8 @@ module "jupyterhub" {
   autopilot_cluster                 = local.enable_autopilot
   workload_identity_service_account = local.jupyter_service_account
 
-  notebook_image         = "us-central1-docker.pkg.dev/ai-on-gke/rag-on-gke/jupyter-notebook-image"
-  notebook_image_tag     = "latest"
+  notebook_image     = "us-central1-docker.pkg.dev/ai-on-gke/rag-on-gke/jupyter-notebook-image"
+  notebook_image_tag = "latest"
 
   db_secret_name         = module.cloudsql.db_secret_name
   cloudsql_instance_name = local.cloudsql_instance
