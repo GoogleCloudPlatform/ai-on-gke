d4d3977d8e439efc08e660f0f03afe8aee25c085
imreddy13 <132504814+imreddy13@users.noreply.github.com>
2024-04-02 14:25:45 -0700

    Add outputs for cluster name and location to be used in the Marketplace UI (#527)

--- applications/jupyter/outputs.tf
+++ applications/jupyter/outputs.tf
@@ -37,3 +37,18 @@ output "kubernetes_namespace" {
   value       = local.kubernetes_namespace
   description = "Kubernetes namespace"
 }
+
+output "gke_cluster_name" {
+  value       = var.cluster_name
+  description = "GKE cluster name"
+}
+
+output "gke_cluster_location" {
+  value       = var.cluster_location
+  description = "GKE cluster location"
+}
+
+output "project_id" {
+  value       = var.project_id
+  description = "GKE cluster location"
+}
--- applications/rag/outputs.tf
+++ applications/rag/outputs.tf
@@ -58,6 +58,21 @@ output "kubernetes_namespace" {
   description = "Kubernetes namespace"
 }
 
+output "gke_cluster_name" {
+  value       = var.cluster_name
+  description = "GKE cluster name"
+}
+
+output "gke_cluster_location" {
+  value       = var.cluster_location
+  description = "GKE cluster location"
+}
+
+output "project_id" {
+  value       = var.project_id
+  description = "GKE cluster location"
+}
+
 output "gcp_network" {
   value       = local.network_name
   description = "Provisioned GCP Network Name"
