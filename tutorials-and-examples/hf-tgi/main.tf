928da5de7380b224f19e7dc2fbf24ef7da7fa7fa
Max Gong <gongmax@google.com>
2024-04-26 01:58:48 +0000

    fix comment

--- tutorials-and-examples/hf-tgi/main.tf
+++ tutorials-and-examples/hf-tgi/main.tf
@@ -47,6 +47,9 @@ resource "kubernetes_service" "inference_service" {
 }
 
 resource "kubernetes_deployment" "inference_deployment" {
+  timeouts {
+    create = "30m"
+  }
   metadata {
     name      = "mistral-7b-instruct"
     namespace = var.namespace
