b39827c96352ed86c6fe702dee49abb5e5d44e09
Vivian Wu <vivianrwu@google.com>
2024-04-26 23:26:10 +0000

    edit http server description

--- tutorials-and-examples/inference-servers/jetstream/http-server/http_server.py
+++ tutorials-and-examples/inference-servers/jetstream/http-server/http_server.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""HTTP Server to interact with MaxText + JetStream Server."""
+"""HTTP Server to interact with JetStream Server."""
 
 import asyncio
 import concurrent.futures
