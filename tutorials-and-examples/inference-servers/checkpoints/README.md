4660bd06838a904628e3a92de1ca77bc5fa96879
vivianrwu <vivianrwu@google.com>
2024-05-03 10:08:49 -0700

    Add soruce files for Jestream Pytorch on GKE single host user guide (#625)
    * Add soruce files for Jestream Pytorch single host userguide
    
    * Add differentiation for model server arguments
    
    * Add XLA for Pytorch title
    
    * add docker image for jetstream-pytorch-server
    
    * update arguments for checkpoint conversion job
    
    * use stable jetstream version for checkpoint conversion and jetstream-pytorch server
    
    * fix script variables

--- tutorials-and-examples/inference-servers/checkpoints/README.md
+++ tutorials-and-examples/inference-servers/checkpoints/README.md
@@ -11,8 +11,20 @@ docker push gcr.io/${PROJECT_ID}/inference-checkpoint:latest
 
 Now you can use it in a [Kubernetes job](../jetstream/maxtext/single-host-inference/checkpoint-job.yaml) and pass the following arguments
 
+Jetstream + MaxText
 ```
 - -i=INFERENCE_SERVER
 - -b=BUCKET_NAME
 - -m=MODEL_PATH
+- -v=VERSION (Optional)
+```
+
+Jetstream + Pytorch/XLA
+```
+- -i=INFERENCE_SERVER
+- -m=MODEL_PATH
+- -q=QUANTIZE (Optional)
+- -v=VERSION
+- -1=EXTRA_PARAM_1
+- -2=EXTRA_PARAM_2
 ```
--- tutorials-and-examples/inference-servers/checkpoints/checkpoint_converter.sh
+++ tutorials-and-examples/inference-servers/checkpoints/checkpoint_converter.sh
@@ -6,19 +6,34 @@ BUCKET_NAME=""
 MODEL_PATH=""
 
 print_usage() {
-    printf "Usage: $0 [ -b BUCKET_NAME ] [ -i INFERENCE_SERVER ] [ -m MODEL_PATH ] [ -v VERSION ]"
+    printf "Usage: $0 [ -b BUCKET_NAME ] [ -i INFERENCE_SERVER ] [ -m MODEL_PATH ] [ -q QUANTIZE ] [ -v VERSION ] [ -1 EXTRA_PARAM_1 ] [ -2 EXTRA_PARAM_2 ]"
 }
 
 print_inference_server_unknown() {
     printf "Enter a valid inference server [ -i INFERENCE_SERVER ]"
-    printf "Valid options: jetstream-maxtext"
+    printf "Valid options: jetstream-maxtext, jetstream-pytorch"
+}
+
+check_gsbucket() {
+    BUCKET_NAME=$1
+    if [ -z $BUCKET_NAME ]; then
+        echo "BUCKET_NAME is empty, please provide a GSBucket"
+    fi
+}
+
+check_model_path() {
+    MODEL_PATH=$1
+    if [ -z $MODEL_PATH ]; then
+        echo "MODEL_PATH is empty, please provide the model path"
+    fi
 }
 
 download_kaggle_checkpoint() {
     BUCKET_NAME=$1
-    MODEL_NAME=$2
-    VARIATION_NAME=$3
-    MODEL_PATH=$4
+    MODEL_PATH=$2
+    export MODEL_NAME=$(echo ${MODEL_PATH} | awk -F'/' '{print $2}')
+    export VARIATION_NAME=$(echo ${MODEL_PATH} | awk -F'/' '{print $4}')
+    export MODEL_SIZE=$(echo ${VARIATION_NAME} | awk -F'-' '{print $1}')
 
     mkdir -p /data/${MODEL_NAME}_${VARIATION_NAME}
     kaggle models instances versions download ${MODEL_PATH} --untar -p /data/${MODEL_NAME}_${VARIATION_NAME}
@@ -56,36 +71,79 @@ convert_maxtext_checkpoint() {
     echo -e "\nCompleted unscanning checkpoint to gs://${BUCKET_NAME}/final/unscanned/${MODEL_NAME}_${VARIATION_NAME}/${RUN_NAME}/checkpoints/0/items"
 }
 
+convert_pytorch_checkpoint() {
+    MODEL_PATH=$1
+    INPUT_CKPT_DIR=$2
+    OUTPUT_CKPT_DIR=$3
+    QUANTIZE=$4
+    PYTORCH_VERSION=$5
+    JETSTREAM_VERSION=v0.2.0
 
-while getopts 'b:i:m:' flag; do
+    if [ -z $PYTORCH_VERSION ]; then
+        PYTORCH_VERSION=jetstream-v0.2.0
+    fi
+
+    CKPT_PATH="$(echo ${INPUT_CKPT_DIR} | awk -F'gs://' '{print $2}')"
+    BUCKET_NAME="$(echo ${CKPT_PATH} | awk -F'/' '{print $1}')"
+
+    TO_REPLACE=gs://${BUCKET_NAME}
+    INPUT_CKPT_DIR_LOCAL=${INPUT_CKPT_DIR/${TO_REPLACE}/${MODEL_PATH}}
+    OUTPUT_CKPT_DIR_LOCAL=/pt-ckpt/
+
+    if [ -z $QUANTIZE ]; then
+        QUANTIZE="False"
+    fi
+
+    git clone https://github.com/google/JetStream.git
+    git clone https://github.com/google/jetstream-pytorch.git
+    cd JetStream
+    git checkout ${JETSTREAM_VERSION}
+    pip install -e
+
+    # checkout stable Pytorch commit
+    cd ../jetstream-pytorch
+    git checkout ${PYTORCH_VERSION}
+    bash install_everything.sh
+    export PYTHONPATH=$PYTHONPATH:$(pwd)/deps/xla/experimental/torch_xla2:$(pwd)/JetStream:$(pwd)
+
+    echo -e "\nRunning conversion script to convert model weights. This can take a couple minutes..."
+    python3 -m convert_checkpoints --input_checkpoint_dir=${INPUT_CKPT_DIR_LOCAL} --output_checkpoint_dir=${OUTPUT_CKPT_DIR_LOCAL} --quantize=${QUANTIZE}
+    
+    echo -e "\nCompleted conversion of checkpoint to ${OUTPUT_CKPT_DIR_LOCAL}"
+    echo -e "\nUploading converted checkpoint from local path ${OUTPUT_CKPT_DIR_LOCAL} to GSBucket ${OUTPUT_CKPT_DIR}"
+
+    gcloud storage cp -r ${OUTPUT_CKPT_DIR_LOCAL}/* ${OUTPUT_CKPT_DIR}
+    echo -e "\nCompleted uploading converted checkpoint from local path ${OUTPUT_CKPT_DIR_LOCAL} to GSBucket ${OUTPUT_CKPT_DIR}"
+}
+
+
+while getopts 'b:i:m:q:v:1:2:' flag; do
     case "${flag}" in
         b) BUCKET_NAME="$(echo ${OPTARG} | awk -F'=' '{print $2}')" ;;
         i) INFERENCE_SERVER="$(echo ${OPTARG} | awk -F'=' '{print $2}')" ;;
         m) MODEL_PATH="$(echo ${OPTARG} | awk -F'=' '{print $2}')" ;;
+        q) QUANTIZE="$(echo ${OPTARG} | awk -F'=' '{print $2}')" ;;
         v) VERSION="$(echo ${OPTARG} | awk -F'=' '{print $2}')" ;;
+        1) EXTRA_PARAM_1="$(echo ${OPTARG} | awk -F'=' '{print $2}')" ;;
+        2) EXTRA_PARAM_2="$(echo ${OPTARG} | awk -F'=' '{print $2}')" ;;
         *) print_usage
     exit 1 ;;
     esac
 done
 
-if [ -z $BUCKET_NAME ]; then
-    echo "BUCKET_NAME is empty, please provide a GSBucket"
-    fi
-
-if [ -z $MODEL_PATH ]; then
-    echo "MODEL_PATH is empty, please provide the model path"
-    fi
-
 echo "Inference server is ${INFERENCE_SERVER}"
-MODEL_NAME=$(echo ${MODEL_PATH} | awk -F'/' '{print $2}')
-VARIATION_NAME=$(echo ${MODEL_PATH} | awk -F'/' '{print $4}')
-MODEL_SIZE=$(echo ${VARIATION_NAME} | awk -F'-' '{print $1}')
 
 case ${INFERENCE_SERVER} in 
 
     jetstream-maxtext)
-        download_kaggle_checkpoint "$BUCKET_NAME" "$MODEL_NAME" "$VARIATION_NAME" "$MODEL_PATH"
+        check_gsbucket "$BUCKET_NAME"
+        check_model_path "$MODEL_PATH"
+        download_kaggle_checkpoint "$BUCKET_NAME" "$MODEL_PATH"
         convert_maxtext_checkpoint "$BUCKET_NAME" "$MODEL_NAME" "$VARIATION_NAME" "$MODEL_SIZE" "$VERSION"
+        ;;
+    jetstream-pytorch)
+    check_model_path "$MODEL_PATH"
+        convert_pytorch_checkpoint "$MODEL_PATH" "$EXTRA_PARAM_1" "$EXTRA_PARAM_2" "$QUANTIZE" "$VERSION"
         ;;
     *) print_inference_server_unknown
 exit 1 ;;
