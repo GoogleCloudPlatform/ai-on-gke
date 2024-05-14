3e15dfad824b721d4c895ac5e9ff39022f984194
Grant Sorbo <gtsorbo@google.com>
2024-05-14 17:00:47 +0000

    fix: remove output

--- best-practices/ml-platform/terraform/modules/node-pools/outputs.tf
+++ best-practices/ml-platform/terraform/modules/node-pools/outputs.tf
@@ -12,6 +12,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-output "nodepool_sa_email" {
-  value = google_service_account.nodepool_sa.email
-}
