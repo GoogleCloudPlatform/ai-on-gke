fb8282e1c997c3e0e28ab5140898d8b2ccda448a
arueth <rueth@google.com>
2024-05-29 03:26:23 +0000

    Updated google providers to 5.31.0

--- best-practices/ml-platform/examples/platform/playground/versions.tf
+++ best-practices/ml-platform/examples/platform/playground/versions.tf
@@ -18,11 +18,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "5.19.0"
+      version = "5.31.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "5.19.0"
+      version = "5.31.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
