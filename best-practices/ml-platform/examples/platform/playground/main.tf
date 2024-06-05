93be74ac209d01c503940c661c77f0f5b6e78127
arueth <rueth@google.com>
2024-06-05 03:40:47 +0000

    Moved fleet resources to a new file

--- best-practices/ml-platform/examples/platform/playground/main.tf
+++ best-practices/ml-platform/examples/platform/playground/main.tf
@@ -20,81 +20,6 @@ locals {
   kubeconfig_dir           = abspath("${path.module}/kubeconfig")
 }
 
-# FLEET
-##########################################################################
-resource "google_gke_hub_feature" "configmanagement" {
-  depends_on = [
-    google_project_service.anthos_googleapis_com,
-    google_project_service.anthosconfigmanagement_googleapis_com,
-    google_project_service.compute_googleapis_com,
-    google_project_service.gkeconnect_googleapis_com,
-    google_project_service.gkehub_googleapis_com,
-    local.configsync_repository
-  ]
-
-  location = "global"
-  name     = "configmanagement"
-  project  = data.google_project.environment.project_id
-}
-
-resource "google_gke_hub_membership" "cluster" {
-  depends_on = [
-    google_gke_hub_feature.configmanagement,
-    google_project_service.gkeconnect_googleapis_com,
-    google_project_service.gkehub_googleapis_com
-  ]
-
-  membership_id = google_container_cluster.mlp.name
-  project       = data.google_project.environment.project_id
-
-  endpoint {
-    gke_cluster {
-      resource_link = "//container.googleapis.com/${google_container_cluster.mlp.id}"
-    }
-  }
-}
-
-resource "google_gke_hub_feature_membership" "cluster_configmanagement" {
-  depends_on = [
-    google_container_node_pool.system,
-    google_project_service.anthos_googleapis_com,
-    google_project_service.anthosconfigmanagement_googleapis_com,
-    google_project_service.gkeconnect_googleapis_com,
-    google_project_service.gkehub_googleapis_com,
-    local.configsync_repository,
-    module.cloud-nat
-  ]
-
-  feature    = "configmanagement"
-  location   = "global"
-  membership = google_gke_hub_membership.cluster.membership_id
-  project    = data.google_project.environment.project_id
-
-  configmanagement {
-    version = var.config_management_version
-
-    config_sync {
-      source_format = "unstructured"
-
-      git {
-        policy_dir  = "manifests/clusters"
-        secret_type = "token"
-        sync_branch = local.configsync_repository.default_branch
-        sync_repo   = local.configsync_repository.http_clone_url
-      }
-    }
-
-    policy_controller {
-      enabled                    = true
-      referential_rules_enabled  = true
-      template_library_installed = true
-
-    }
-  }
-}
-
-
-
 # GIT REPOSITORY
 ##########################################################################
 module "configsync_repository" {
@@ -109,8 +34,3 @@ module "configsync_repository" {
   owner       = var.github_org
   token       = var.github_token
 }
-
-
-
-# KUBERNETES
-##########################################################################
