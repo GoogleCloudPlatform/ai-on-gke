12eacd81c32c90438f7b5c47559203cc24a0e323
arueth <rueth@google.com>
2024-06-11 14:37:49 +0000

    Tweaked test scripts

--- best-practices/ml-platform/test/scripts/helpers/dataprocessing_cleanup.sh
+++ best-practices/ml-platform/test/scripts/helpers/dataprocessing_cleanup.sh
@@ -14,27 +14,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-echo_title "Removing IAM permissions"
-
-gcloud projects remove-iam-policy-binding ${MLP_PROJECT_ID} \
-    --member "serviceAccount:wi-ml-team-ray-head@${PROJECT_ID}.iam.gserviceaccount.com" \
-    --role roles/storage.objectViewer
-
-gcloud projects remove-iam-policy-binding ${PROJECT_ID} \
-    --member "serviceAccount:wi-ml-team-ray-worker@${PROJECT_ID}.iam.gserviceaccount.com" \
-    --role roles/storage.objectAdmin
-
 echo_title "Deleting data processing Artifact Registry repository"
 
 gcloud artifacts repositories delete dataprocessing \
     --location=us \
-    --project=${PROJECT_ID} \
+    --project=${MLP_PROJECT_ID} \
     --quiet
 
 echo_title "Deleting dataprocessing GCS buckets"
 
 gsutil -m -q rm -rf gs://${PROCESSING_BUCKET}/*
-gcloud storage buckets delete gs://${PROCESSING_BUCKET} --project ${PROJECT_ID}
+gcloud storage buckets delete gs://${PROCESSING_BUCKET} --project ${MLP_PROJECT_ID}
 
-gsutil -m -q rm -rf gs://${PROJECT_ID}_cloudbuild/*
-gcloud storage buckets delete gs://${PROJECT_ID}_cloudbuild --project ${PROJECT_ID}
+gsutil -m -q rm -rf gs://${MLP_PROJECT_ID}_cloudbuild/*
+gcloud storage buckets delete gs://${MLP_PROJECT_ID}_cloudbuild --project ${MLP_PROJECT_ID}
