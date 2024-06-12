3b2b86f713c4512a8995e68f62a271082da46d72
arueth <rueth@google.com>
2024-06-12 20:18:07 +0000

    Added a100 40GB node pools

--- best-practices/ml-platform/examples/platform/playground/container_node_pool.tf
+++ best-practices/ml-platform/examples/platform/playground/container_node_pool.tf
@@ -83,6 +83,184 @@ resource "google_container_node_pool" "cpu_n2s8" {
 # GPU
 # Available zones: https://cloud.google.com/compute/docs/gpus/gpu-regions-zones#view-using-table
 ###############################################################################
+resource "google_container_node_pool" "gpu_a100x2_a2h2" {
+  depends_on = [google_container_cluster.mlp]
+
+  # Variables
+  cluster  = google_container_cluster.mlp.name
+  location = var.subnet_01_region
+  name     = "gpu-a100x2-a2h2"
+  node_locations = [
+    "${var.subnet_01_region}-a",
+    "${var.subnet_01_region}-b",
+    "${var.subnet_01_region}-c",
+    "${var.subnet_01_region}-f"
+  ]
+  project = data.google_project.environment.project_id
+
+  # Blocks
+  autoscaling {
+    location_policy      = "ANY"
+    total_max_node_count = 1000
+    total_min_node_count = 0
+  }
+
+  lifecycle {
+    ignore_changes = [
+      node_config[0].labels,
+      node_config[0].taint,
+    ]
+  }
+
+  network_config {
+    enable_private_nodes = true
+  }
+
+  node_config {
+    # Variables
+    labels = {
+      "resource-model" : "a100"
+      "resource-type" : "gpu"
+    }
+    machine_type    = "a2-highgpu-2g"
+    service_account = google_service_account.cluster.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+
+    # Blocks
+    gcfs_config {
+      enabled = true
+    }
+
+    guest_accelerator {
+      count = 2
+      type  = "nvidia-tesla-a100"
+
+      gpu_driver_installation_config {
+        gpu_driver_version = var.gpu_driver_version
+      }
+    }
+
+    gvnic {
+      enabled = true
+    }
+
+    reservation_affinity {
+      consume_reservation_type = "NO_RESERVATION"
+    }
+
+    shielded_instance_config {
+      enable_integrity_monitoring = true
+      enable_secure_boot          = true
+    }
+
+    taint {
+      effect = "NO_SCHEDULE"
+      key    = "on-demand"
+      value  = true
+    }
+  }
+
+  timeouts {
+    create = "30m"
+    update = "20m"
+  }
+}
+
+###############################################################################
+
+resource "google_container_node_pool" "gpu_a100x2_a2h2_dws" {
+  depends_on = [google_container_cluster.mlp]
+
+  # Variables
+  cluster  = google_container_cluster.mlp.name
+  location = var.subnet_01_region
+  name     = "gpu-a100x2-a2h2-dws"
+  node_locations = [
+    "${var.subnet_01_region}-a",
+    "${var.subnet_01_region}-b",
+    "${var.subnet_01_region}-c",
+    "${var.subnet_01_region}-f"
+  ]
+  project = data.google_project.environment.project_id
+
+  # Blocks
+  autoscaling {
+    location_policy      = "ANY"
+    total_max_node_count = 1000
+    total_min_node_count = 0
+  }
+
+  lifecycle {
+    ignore_changes = [
+      node_config[0].labels,
+      node_config[0].taint,
+    ]
+  }
+
+  network_config {
+    enable_private_nodes = true
+  }
+
+  node_config {
+    # Variables
+    labels = {
+      "resource-model" : "a100"
+      "resource-type" : "gpu"
+    }
+    machine_type    = "a2-highgpu-2g"
+    service_account = google_service_account.cluster.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+
+    # Blocks
+    gcfs_config {
+      enabled = true
+    }
+
+    guest_accelerator {
+      count = 2
+      type  = "nvidia-tesla-a100"
+
+      gpu_driver_installation_config {
+        gpu_driver_version = var.gpu_driver_version
+      }
+    }
+
+    gvnic {
+      enabled = true
+    }
+
+    reservation_affinity {
+      consume_reservation_type = "NO_RESERVATION"
+    }
+
+    shielded_instance_config {
+      enable_integrity_monitoring = true
+      enable_secure_boot          = true
+    }
+
+    taint {
+      effect = "NO_SCHEDULE"
+      key    = "on-demand"
+      value  = true
+    }
+  }
+
+  queued_provisioning {
+    enabled = true
+  }
+
+  timeouts {
+    create = "30m"
+    update = "20m"
+  }
+}
+
+###############################################################################
+
 resource "google_container_node_pool" "gpu_h100x8_a3h8_dws" {
   depends_on = [google_container_cluster.mlp]
 
