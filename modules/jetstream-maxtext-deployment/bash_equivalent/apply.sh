bbae0d9555ae396c3392248c0f6d4b79c691e52d
Brendan Slabe <slabe@google.com>
2024-06-24 18:44:46 +0000

    podmonitoring

--- modules/jetstream-maxtext-deployment/bash_equivalent/apply.sh
+++ modules/jetstream-maxtext-deployment/bash_equivalent/apply.sh
@@ -3,7 +3,24 @@ if [ -z "$BUCKET_NAME" ]; then
     exit 2;
 fi
 
-jq \
-    --arg LOAD_PARAMETERS_ARG "load_parameters=gs://$BUCKET_NAME/final/unscanned/gemma_7b-it/0/checkpoints/0/items" \
-    '.spec.template.spec.containers[0].args += [$LOAD_PARAMETERS_ARG]' \
-deployment.json | kubectl apply -f -
+JETSTREAM_MANIFEST="$(cat deployment.json)"
+PODMONITORING_MANIFEST="$(cat podmonitoring.json)"
+
+if [ "$METRICS_PORT" != "" ]; then
+    PODMONITORING_MANIFEST="$(echo "$PODMONITORING_MANIFEST" \
+    | jq \
+        --arg METRICS_PORT "$METRICS_PORT" \
+        '.spec.endpoints[0].port = $METRICS_PORT')"
+    JETSTREAM_MANIFEST="$(echo "$JETSTREAM_MANIFEST" \
+    |  jq \
+        --arg METRICS_PORT_ARG "prometheus_port=$METRICS_PORT" \
+        '.spec.template.spec.containers[0].args += [$METRICS_PORT_ARG]')"
+fi
+
+JETSTREAM_MANIFEST="$(echo "$JETSTREAM_MANIFEST" \
+    |  jq \
+        --arg LOAD_PARAMETERS_ARG "load_parameters=gs://$BUCKET_NAME/final/unscanned/gemma_7b-it/0/checkpoints/0/items" \
+        '.spec.template.spec.containers[0].args += [$LOAD_PARAMETERS_ARG]')"
+
+kubectl apply -f -$("$JETSTREAM_MANIFEST")
+kubectl apply -f -$("$PODMONITORING_MANIFEST")
