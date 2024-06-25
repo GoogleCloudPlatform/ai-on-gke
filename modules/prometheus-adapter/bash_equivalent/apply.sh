236cb769c0033b645167825c8cf87ff2133f790a
Brendan Slabe <slabe@google.com>
2024-06-25 16:34:37 +0000

    prometheus-adapter

--- modules/prometheus-adapter/bash_equivalent/apply.sh
+++ modules/prometheus-adapter/bash_equivalent/apply.sh
@@ -1,7 +1,25 @@
+if [ -z "$PROJECT_ID" ]; then
+    echo "Must provide PROJECT_ID in environment" 1>&2
+    exit 2;
+fi
+
 if [ -z "$RELEASE_NAME" ]; then
     echo "Must provide RELEASE_NAME in environment" 1>&2
     exit 2;
 fi
+
+# Adapted from https://raw.githubusercontent.com/GoogleCloudPlatform/prometheus-engine/v0.10.0/examples/frontend.yaml
+PROMETHEUS_FRONTEND_MANIFEST="$(cat deployment-frontend.json)"
+PROMETHEUS_SERVICE_MANIFEST="$(cat service-prometheus.json)"
+
+PROMETHEUS_FRONTEND_MANIFEST="$(echo "$PROMETHEUS_FRONTEND_MANIFEST" \
+  | jq \
+      --arg PROJECT_ID_ARG "--query.project-id=$PROJECT_ID" \
+      '.spec.template.spec.containers[0].args += [$PROJECT_ID_ARG]' \
+    )"
+
+echo $PROMETHEUS_FRONTEND_MANIFEST | kubectl apply -f -
+echo $PROMETHEUS_SERVICE_MANIFEST | kubectl apply -f -
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
