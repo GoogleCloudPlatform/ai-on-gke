0d5bfedcf08998e774f555058bb73e99e40c4397
Brendan Slabe <slabe@google.com>
2024-06-27 22:42:45 +0000

    first commit

---
+++ modules/prometheus-adapter/bash_equivalent/README.md
@@ -0,0 +1,11 @@
+This module requires the cluster name to be passed in manually via the CLUSTER_NAME variable to filter incoming metrics. This is a consequence of differing cluster name schemas between GKE and standard k8s clusters. Instructions for each are as follows for if the cluster name isnt already known.
+
+## For GKE clusters
+
+Remove any charachters prior to and including the last underscore:
+`kubectl config current-context | awk -F'_' ' { print $NF }'`
+
+## For other clusters
+
+The cluster name is simply:
+ `kubectl config current-context`
---
+++ modules/prometheus-adapter/bash_equivalent/apply.sh
@@ -0,0 +1,43 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if [ -z "$PROJECT_ID" ]; then
+    echo "Must provide PROJECT_ID in environment" 1>&2
+    exit 2;
+fi
+
+if [ -z "$CLUSTER_NAME" ]; then
+    echo "Must provide CLUSTER_NAME in environment" 1>&2
+    exit 2;
+fi
+
+# Adapted from https://raw.githubusercontent.com/GoogleCloudPlatform/prometheus-engine/v0.10.0/examples/frontend.yaml
+PROMETHEUS_FRONTEND_MANIFEST="$(cat ./deployment-frontend.json)"
+PROMETHEUS_SERVICE_MANIFEST="$(cat ./service-prometheus.json)"
+
+PROMETHEUS_FRONTEND_MANIFEST="$(echo "$PROMETHEUS_FRONTEND_MANIFEST" \
+  | jq \
+      --arg PROJECT_ID_ARG "--query.project-id=$PROJECT_ID" \
+      '.spec.template.spec.containers[0].args += [$PROJECT_ID_ARG]' \
+    )"
+
+echo $PROMETHEUS_FRONTEND_MANIFEST | kubectl apply -f -
+echo $PROMETHEUS_SERVICE_MANIFEST | kubectl apply -f -
+
+PROMETHEUS_HELM_VALUES_FILE=$(mktemp)
+sed "s/\${cluster_name}/$CLUSTER_NAME/g" ../templates/values.yaml.tftpl >> "$PROMETHEUS_HELM_VALUES_FILE"
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install jetstream prometheus-community/prometheus-adapter -f "$PROMETHEUS_HELM_VALUES_FILE"
