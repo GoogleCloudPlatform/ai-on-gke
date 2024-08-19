02f866e60a0901eb84528c62ded97e4901381b15
arueth <rueth@google.com>
2024-08-19 19:41:07 +0000

    Converted fine tuning to use structured JSON logging and enhanced logging

---
+++ best-practices/ml-platform/examples/use-case/finetuning/pytorch/DEVELOPER.md
@@ -0,0 +1,114 @@
+# Fine Tuning PyTorch Developer Guide
+
+- Install [`pyenv`](https://github.com/pyenv/pyenv?tab=readme-ov-file#installation)
+
+- Install the `python` version
+
+  ```
+  pyenv install 3.10.14
+  ```
+
+- Clone the repository
+
+  ```
+  git clone https://github.com/GoogleCloudPlatform/ai-on-gke.git && \
+  cd ai-on-gke
+  ```
+
+- Change directory to the `src` directory
+
+  ```
+  cd best-practices/ml-platform/examples/use-case/finetuning/pytorch/src
+  ```
+
+- Set the local `python` version
+
+  ```
+  pyenv local 3.10.14
+  ```
+
+- Create a virtual environment
+
+  ```
+  python -m venv venv
+  ```
+
+- Activate the virtual environment
+
+  ```
+  source venv/bin/activate
+  ```
+
+- Install the requirements
+
+  ```
+  pip install --no-cache-dir -r requirements.txt
+  ```
+
+- Set environment variables
+
+  ```
+  export MLP_ENVIRONMENT_NAME=<environment-name>
+  export MLP_PROJECT_ID=<project-id>
+  export HF_TOKEN_FILE=${HOME}/secrets/mlp-hugging-face-token
+
+  export PROJECT_ID=${MLP_PROJECT_ID}
+  gcloud config set project ${PROJECT_ID}
+  export PROJECT_NUMBER=$(gcloud projects describe ${PROJECT_ID} --format="value(projectNumber)")
+
+  export TRAINING_DATASET_BUCKET="${PROJECT_ID}-${MLP_ENVIRONMENT_NAME}-processing"
+  export MODEL_BUCKET="${PROJECT_ID}-${MLP_ENVIRONMENT_NAME}-model"
+  export CLUSTER_NAME="mlp-${MLP_ENVIRONMENT_NAME}"
+  export NAMESPACE=ml-team
+  export KSA=default
+  export HF_TOKEN=$(tr --delete '\n' <${HF_TOKEN_FILE})
+  export DOCKER_IMAGE_URL="us-docker.pkg.dev/${PROJECT_ID}/${MLP_ENVIRONMENT_NAME}-llm-finetuning/finetune:v1.0.0"
+
+  export EXPERIMENT=""
+  export MLFLOW_ENABLE="false"
+  export MLFLOW_ENABLE_SYSTEM_METRICS_LOGGING="false"
+  export MLFLOW_TRACKING_URI=""
+  export TRAINING_DATASET_PATH="dataset/output/training"
+  export MODEL_PATH="/model-data/model-gemma2/experiment"
+  export MODEL_NAME="google/gemma-2-9b-it"
+  export ACCELERATOR="l4"
+  ```
+
+- Install `gcsfuse`
+
+  ```
+  export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s` && \
+  echo "deb [signed-by=/usr/share/keyrings/cloud.google.asc] https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list && \
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.asc && \
+  sudo apt-get update && \
+  sudo apt-get install -y gcsfuse
+  ```
+
+- Create the model bucket mount directory
+
+  ```
+  export MOUNT_DIR="/model-data"
+  sudo mkdir -p ${MOUNT_DIR}
+  sudo chown ${USER} ${MOUNT_DIR}
+  ```
+
+- Mount your model bucket using `gcsfuse`
+
+  ```
+  gcsfuse ${MODEL_BUCKET} ${MOUNT_DIR}
+  ```
+
+- Run the `fine_tune.py` script
+
+  ```
+  accelerate launch \
+  --config_file fsdp_config.yaml \
+  --debug \
+  fine_tune.py
+  ```
+
+- Unmount the model bucket
+
+  ```
+  fusermount -u ${MOUNT_DIR}
+  ```
--- best-practices/ml-platform/examples/use-case/finetuning/pytorch/src/fine_tune.py
+++ best-practices/ml-platform/examples/use-case/finetuning/pytorch/src/fine_tune.py
@@ -1,55 +1,40 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datasets
+import logging
 import os
+import signal
+import sys
 import torch
-import logging.config
+import transformers
+
 from accelerate import Accelerator
-
-from datasets import load_dataset, Dataset
-from transformers import (
-    AutoModelForCausalLM,
-    AutoTokenizer,
-)
+from datasets import Dataset, load_dataset, load_from_disk
 from peft import LoraConfig, PeftModel
-from trl import SFTConfig
-from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
-from datasets import load_from_disk
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from trl import DataCollatorForCompletionOnlyLM, SFTConfig, SFTTrainer
 
-logging.config.fileConfig("logging.conf")
-logger = logging.getLogger("finetune")
-logger.debug(logger)
+from custom_json_formatter import CustomJSONFormatter
 
-if "MLFLOW_ENABLE" in os.environ and os.environ.get("MLFLOW_ENABLE") == "true":
-    import mlflow
 
-    remote_server_uri = os.environ.get("MLFLOW_TRACKING_URI")
-    mlflow.set_tracking_uri(remote_server_uri)
+def graceful_shutdown(signal_number, stack_frame):
+    signal_name = signal.Signals(signal_number).name
 
-    experiment = os.environ.get("EXPERIMENT")
-
-    mlflow.set_experiment(experiment)
-    mlflow.autolog()
-
-accelerator = Accelerator()
-
-# The bucket which contains the training data
-training_data_bucket = os.environ.get("TRAINING_DATASET_BUCKET")
-
-training_data_path = os.environ.get("TRAINING_DATASET_PATH")
-
-# The model that you want to train from the Hugging Face hub
-model_name = os.environ.get("MODEL_NAME")
-
-# Fine-tuned model name
-new_model = os.environ.get("NEW_MODEL")
-
-# The root path of where the fine-tuned model will be saved
-save_model_path = os.environ.get("MODEL_PATH")
-
-# Load tokenizer
-tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-tokenizer.pad_token = tokenizer.eos_token
-tokenizer.padding_side = "right"  # Fix weird overflow issue with fp16 training
-
-EOS_TOKEN = tokenizer.eos_token
+    logger.info(f"Received {signal_name}({signal_number}), shutting down...")
+    # TODO: Add logic to handled checkpointing if required
+    sys.exit(0)
 
 
 def formatting_prompts_func(example):
@@ -60,201 +45,279 @@ def formatting_prompts_func(example):
     return {"prompts": output_texts}
 
 
-training_dataset = load_from_disk(f"gs://{training_data_bucket}/{training_data_path}")
+if __name__ == "__main__":
+    # Configure logging
+    logger = logging.getLogger("finetune")
+    LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
+    print(f"LOG_LEVEL: {LOG_LEVEL}")
+    logger.setLevel(LOG_LEVEL)
 
-logger.info("Data Formatting Started")
-input_data = training_dataset.map(formatting_prompts_func, batched=True)
-logger.info("Data Formatting Completed")
+    handler = logging.StreamHandler()
+    handler.setFormatter(CustomJSONFormatter())
+    handler.setLevel(LOG_LEVEL)
+    logger.addHandler(handler)
 
-INPUT_OUTPUT_DELIMITER = "<start_of_turn>model"
+    # For local testing you can enable logging to a file
+    # file_handler = logging.FileHandler('fine_tuning.log')
+    # file_handler.setFormatter(CustomJSONFormatter())
+    # file_handler.setLevel(LOG_LEVEL)
+    # logger.addHandler(file_handler)
 
-collator = DataCollatorForCompletionOnlyLM(INPUT_OUTPUT_DELIMITER, tokenizer=tokenizer)
+    datasets.disable_progress_bar()
+    transformers.utils.logging.disable_progress_bar()
 
-################################################################################
-# QLoRA parameters
-################################################################################
+    logger.info("Configure signal handlers")
+    signal.signal(signal.SIGINT, graceful_shutdown)
+    signal.signal(signal.SIGTERM, graceful_shutdown)
 
-# LoRA attention dimension
-lora_r = int(os.getenv("LORA_R", "8"))
+    if "MLFLOW_ENABLE" in os.environ and os.environ["MLFLOW_ENABLE"] == "true":
+        import mlflow
 
-# Alpha parameter for LoRA scaling
-lora_alpha = int(os.getenv("LORA_ALPHA", "16"))
+        remote_server_uri = os.environ["MLFLOW_TRACKING_URI"]
+        mlflow.set_tracking_uri(remote_server_uri)
 
-# Dropout probability for LoRA layers
-lora_dropout = float(os.getenv("LORA_DROPOUT", "0.1"))
+        experiment = os.environ["EXPERIMENT"]
+        mlflow.set_experiment(experiment)
+        mlflow.autolog()
 
-################################################################################
-# TrainingArguments parameters
-################################################################################
+    accelerator = Accelerator()
 
-# Output directory where the model predictions and checkpoints will be stored
-# output_dir = "./results"
+    # The bucket which contains the training data
+    training_data_bucket = os.environ["TRAINING_DATASET_BUCKET"]
 
-# Number of training epochs
-num_train_epochs = int(os.getenv("EPOCHS", "1"))
+    training_data_path = os.environ["TRAINING_DATASET_PATH"]
 
-# Enable fp16/bf16 training (set bf16 to True with an A100)
-fp16 = False
-bf16 = False
+    # The model that you want to train from the Hugging Face hub
+    model_name = os.environ["MODEL_NAME"]
 
-# Batch size per GPU for training
-per_device_train_batch_size = int(os.getenv("TRAIN_BATCH_SIZE", "1"))
+    # Fine-tuned model name
+    new_model = os.environ["NEW_MODEL"]
 
-# Batch size per GPU for evaluation
-per_device_eval_batch_size = 1
+    # The root path of where the fine-tuned model will be saved
+    save_model_path = os.environ["MODEL_PATH"]
 
-# Number of update steps to accumulate the gradients for
-gradient_accumulation_steps = int(os.getenv("GRADIENT_ACCUMULATION_STEPS", "1"))
+    # Load tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"  # Fix weird overflow issue with fp16 training
 
-# Enable gradient checkpointing
-gradient_checkpointing = True
+    EOS_TOKEN = tokenizer.eos_token
 
-# Maximum gradient normal (gradient clipping)
-max_grad_norm = float(os.getenv("MAX_GRAD_NORM", "0.3"))
-
-# Initial learning rate (AdamW optimizer)
-learning_rate = float(os.getenv("LEARNING_RATE", "2e-4"))
-
-# Weight decay to apply to all layers except bias/LayerNorm weights
-weight_decay = float(os.getenv("WEIGHT_DECAY", "0.001"))
-
-# Optimizer to use
-optim = "paged_adamw_32bit"
-
-# Learning rate schedule
-lr_scheduler_type = "cosine"
-
-# Number of training steps (overrides num_train_epochs)
-max_steps = -1
-
-# Ratio of steps for a linear warmup (from 0 to learning rate)
-warmup_ratio = float(os.getenv("WARMUP_RATIO", "0.03"))
-
-# Group sequences into batches with same length
-# Saves memory and speeds up training considerably
-group_by_length = True
-
-# Save checkpoint every X updates steps
-save_steps = 0
-
-# Log every X updates steps
-logging_steps = 50
-
-################################################################################
-# SFT parameters
-################################################################################
-
-# Maximum sequence length to use
-max_seq_length = int(os.getenv("MAX_SEQ_LENGTH", "512"))
-
-# Pack multiple short examples in the same input sequence to increase efficiency
-packing = False
-
-# Load base model
-model = AutoModelForCausalLM.from_pretrained(
-    attn_implementation="eager",
-    pretrained_model_name_or_path=model_name,
-    torch_dtype=torch.bfloat16,
-)
-model.config.use_cache = False
-model.config.pretraining_tp = 1
-
-# optimizer = torch.optim.AdamW(model.parameters(), lr=2e-4)
-
-# Load LoRA configuration
-peft_config = LoraConfig(
-    lora_alpha=lora_alpha,
-    lora_dropout=lora_dropout,
-    r=lora_r,
-    bias="none",
-    task_type="CAUSAL_LM",
-    target_modules=[
-        "q_proj",
-        "k_proj",
-        "v_proj",
-        "o_proj",
-        "gate_proj",
-        "up_proj",
-        "down_proj",
-    ],
-)
-
-# Set training parameters
-training_arguments = SFTConfig(
-    dataset_kwargs={
-        "add_special_tokens": False,  # We template with special tokens
-        "append_concat_token": False,  # No need to add additional separator token
-    },
-    output_dir=save_model_path,
-    num_train_epochs=num_train_epochs,
-    per_device_train_batch_size=per_device_train_batch_size,
-    gradient_accumulation_steps=gradient_accumulation_steps,
-    gradient_checkpointing=gradient_checkpointing,
-    gradient_checkpointing_kwargs={"use_reentrant": False},
-    optim=optim,
-    save_steps=save_steps,
-    logging_steps=logging_steps,
-    learning_rate=learning_rate,
-    weight_decay=weight_decay,
-    fp16=fp16,
-    bf16=bf16,
-    max_grad_norm=max_grad_norm,
-    max_steps=max_steps,
-    warmup_ratio=warmup_ratio,
-    group_by_length=group_by_length,
-    lr_scheduler_type=lr_scheduler_type,
-    dataset_text_field="prompts",
-    max_seq_length=max_seq_length,
-    packing=packing,
-)
-
-trainer = SFTTrainer(
-    model=model,
-    args=training_arguments,
-    train_dataset=input_data,
-    tokenizer=tokenizer,
-    peft_config=peft_config,
-    data_collator=collator,
-)
-
-logger.info("Fine tuning started")
-trainer.train()
-logger.info("Fine tuning completed")
-
-if "MLFLOW_ENABLE" in os.environ and os.environ.get("MLFLOW_ENABLE") == "true":
-    mv = mlflow.register_model(
-        model_uri=f"gs://{training_data_bucket}/{save_model_path}", name=new_model
+    training_dataset = load_from_disk(
+        f"gs://{training_data_bucket}/{training_data_path}"
     )
-    logger.info(f"Name: {mv.name}")
-    logger.info(f"Version: {mv.version}")
 
-logger.info("Saving new model")
-trainer.model.save_pretrained(new_model)
+    logger.info("Data Formatting Started")
+    input_data = training_dataset.map(formatting_prompts_func, batched=True)
+    logger.info("Data Formatting Completed")
 
-logger.info("Merging the model with base model")
-# Reload model in FP16 and merge it with LoRA weights
-base_model = AutoModelForCausalLM.from_pretrained(
-    low_cpu_mem_usage=True,
-    pretrained_model_name_or_path=model_name,
-    return_dict=True,
-    torch_dtype=torch.bfloat16,
-)
-model = PeftModel.from_pretrained(base_model, new_model)
+    INPUT_OUTPUT_DELIMITER = "<start_of_turn>model"
 
-model = model.merge_and_unload()
+    collator = DataCollatorForCompletionOnlyLM(
+        INPUT_OUTPUT_DELIMITER, tokenizer=tokenizer
+    )
 
-logger.info("Accelerate unwrap model")
-unwrapped_model = accelerator.unwrap_model(model)
+    ################################################################################
+    # QLoRA parameters
+    ################################################################################
 
-logger.info("Save new model")
-unwrapped_model.save_pretrained(
-    save_model_path,
-    is_main_process=accelerator.is_main_process,
-    save_function=accelerator.save,
-)
+    # LoRA attention dimension
+    lora_r = int(os.getenv("LORA_R", "8"))
 
-logger.info("Save new tokenizer")
-if accelerator.is_main_process:
-    tokenizer.save_pretrained(save_model_path)
+    # Alpha parameter for LoRA scaling
+    lora_alpha = int(os.getenv("LORA_ALPHA", "16"))
 
-logger.info("### Completed ###")
+    # Dropout probability for LoRA layers
+    lora_dropout = float(os.getenv("LORA_DROPOUT", "0.1"))
+
+    ################################################################################
+    # TrainingArguments parameters
+    ################################################################################
+
+    # Output directory where the model predictions and checkpoints will be stored
+    # output_dir = "./results"
+
+    # Number of training epochs
+    num_train_epochs = int(os.getenv("EPOCHS", "1"))
+
+    # Enable fp16/bf16 training (set bf16 to True with an A100)
+    fp16 = False
+    bf16 = False
+
+    # Batch size per GPU for training
+    per_device_train_batch_size = int(os.getenv("TRAIN_BATCH_SIZE", "1"))
+
+    # Batch size per GPU for evaluation
+    per_device_eval_batch_size = 1
+
+    # Number of update steps to accumulate the gradients for
+    gradient_accumulation_steps = int(os.getenv("GRADIENT_ACCUMULATION_STEPS", "1"))
+
+    # Enable gradient checkpointing
+    gradient_checkpointing = True
+
+    # Maximum gradient normal (gradient clipping)
+    max_grad_norm = float(os.getenv("MAX_GRAD_NORM", "0.3"))
+
+    # Initial learning rate (AdamW optimizer)
+    learning_rate = float(os.getenv("LEARNING_RATE", "2e-4"))
+
+    # Weight decay to apply to all layers except bias/LayerNorm weights
+    weight_decay = float(os.getenv("WEIGHT_DECAY", "0.001"))
+
+    # Optimizer to use
+    optim = "paged_adamw_32bit"
+
+    # Learning rate schedule
+    lr_scheduler_type = "cosine"
+
+    # Number of training steps (overrides num_train_epochs)
+    max_steps = -1
+
+    # Ratio of steps for a linear warmup (from 0 to learning rate)
+    warmup_ratio = float(os.getenv("WARMUP_RATIO", "0.03"))
+
+    # Group sequences into batches with same length
+    # Saves memory and speeds up training considerably
+    group_by_length = True
+
+    # Save checkpoint every X updates steps
+    save_steps = 0
+
+    # Log every X updates steps
+    logging_steps = 50
+
+    ################################################################################
+    # SFT parameters
+    ################################################################################
+
+    # Maximum sequence length to use
+    max_seq_length = int(os.getenv("MAX_SEQ_LENGTH", "512"))
+
+    # Pack multiple short examples in the same input sequence to increase efficiency
+    packing = False
+
+    # Load base model
+    logger.info("Loading base model started")
+    model = AutoModelForCausalLM.from_pretrained(
+        attn_implementation="eager",
+        pretrained_model_name_or_path=model_name,
+        torch_dtype=torch.bfloat16,
+    )
+    model.config.use_cache = False
+    model.config.pretraining_tp = 1
+    logger.info("Loading base model completed")
+
+    # optimizer = torch.optim.AdamW(model.parameters(), lr=2e-4)
+
+    logger.info("Configuring fine tuning started")
+    # Load LoRA configuration
+    peft_config = LoraConfig(
+        lora_alpha=lora_alpha,
+        lora_dropout=lora_dropout,
+        r=lora_r,
+        bias="none",
+        task_type="CAUSAL_LM",
+        target_modules=[
+            "q_proj",
+            "k_proj",
+            "v_proj",
+            "o_proj",
+            "gate_proj",
+            "up_proj",
+            "down_proj",
+        ],
+    )
+
+    # Set training parameters
+    training_arguments = SFTConfig(
+        bf16=bf16,
+        dataset_kwargs={
+            "add_special_tokens": False,  # We template with special tokens
+            "append_concat_token": False,  # No need to add additional separator token
+        },
+        dataset_text_field="prompts",
+        disable_tqdm=True,
+        fp16=fp16,
+        gradient_accumulation_steps=gradient_accumulation_steps,
+        gradient_checkpointing=gradient_checkpointing,
+        gradient_checkpointing_kwargs={"use_reentrant": False},
+        group_by_length=group_by_length,
+        log_on_each_node=False,
+        logging_steps=logging_steps,
+        learning_rate=learning_rate,
+        lr_scheduler_type=lr_scheduler_type,
+        max_grad_norm=max_grad_norm,
+        max_seq_length=max_seq_length,
+        max_steps=max_steps,
+        num_train_epochs=num_train_epochs,
+        optim=optim,
+        output_dir=save_model_path,
+        packing=packing,
+        per_device_train_batch_size=per_device_train_batch_size,
+        save_steps=save_steps,
+        warmup_ratio=warmup_ratio,
+        weight_decay=weight_decay,
+    )
+    logger.info("Configuring fine tuning completed")
+
+    logger.info("Creating trainer started")
+    trainer = SFTTrainer(
+        args=training_arguments,
+        data_collator=collator,
+        model=model,
+        peft_config=peft_config,
+        tokenizer=tokenizer,
+        train_dataset=input_data,
+    )
+    logger.info("Creating trainer completed")
+
+    logger.info("Fine tuning started")
+    trainer.train()
+    logger.info("Fine tuning completed")
+
+    if "MLFLOW_ENABLE" in os.environ and os.environ["MLFLOW_ENABLE"] == "true":
+        mv = mlflow.register_model(
+            model_uri=f"gs://{training_data_bucket}/{save_model_path}",
+            name=new_model,
+        )
+        logger.info(f"Name: {mv.name}")
+        logger.info(f"Version: {mv.version}")
+
+    logger.info("Saving new model started")
+    trainer.model.save_pretrained(new_model)
+    logger.info("Saving new model completed")
+
+    logger.info("Merging the new model with base model started")
+    # Reload model in FP16 and merge it with LoRA weights
+    base_model = AutoModelForCausalLM.from_pretrained(
+        low_cpu_mem_usage=True,
+        pretrained_model_name_or_path=model_name,
+        return_dict=True,
+        torch_dtype=torch.bfloat16,
+    )
+    model = PeftModel.from_pretrained(
+        model=base_model,
+        model_id=new_model,
+    )
+    model = model.merge_and_unload()
+    logger.info("Merging the new model with base model completed")
+
+    logger.info("Accelerate unwrap model started")
+    unwrapped_model = accelerator.unwrap_model(model)
+    logger.info("Accelerate unwrap model completed")
+
+    logger.info("Save unwrapped model started")
+    unwrapped_model.save_pretrained(
+        is_main_process=accelerator.is_main_process,
+        save_directory=save_model_path,
+        save_function=accelerator.save,
+    )
+    logger.info("Save unwrapped model completed")
+
+    logger.info("Save new tokenizer started")
+    if accelerator.is_main_process:
+        tokenizer.save_pretrained(save_model_path)
+    logger.info("Save new tokenizer completed")
+
+    logger.info("Script completed")
