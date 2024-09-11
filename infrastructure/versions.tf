3307f4029f504c41c0ee4bf5aa4b459a1c07ca43
Gen Lu <genlu@google.com>
2024-09-11 18:27:43 +0000

    fix: lint
    Change-Id: I61db44be22cc2a82513a700f1f8f1360596c913e

--- infrastructure/versions.tf
+++ infrastructure/versions.tf
@@ -18,8 +18,9 @@ terraform {
       source = "hashicorp/google"
     }
     google-beta = {
-      source  = "hashicorp/google-beta"
-      version = ">= 5.40.0, != 5.44.0, !=6.2.0"
+      source = "hashicorp/google-beta"
+      # Creating Autopilot using GKE submodule is broken in v6.2.0.
+      version = ">= 5.40.0, !=6.2.0"
     }
     helm = {
       source  = "hashicorp/helm"
