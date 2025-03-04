7f3ca7d5a6a2c2fd5d7d4856826a0e07f19b6298
Danjuan Ye <danjuan@google.com>
2025-03-04 20:15:36 +0000

    Add mixtral8 70b NeMo recipe

--- applications/hcc/modules/nemo/recipe.tf
+++ applications/hcc/modules/nemo/recipe.tf
@@ -3,6 +3,7 @@ locals {
     "llama3.1_7b_nemo_pretraining"     = "llama3-7b-fp8.yaml"
     "llama3.1_70b_nemo_pretraining"    = "llama3-70b-fp8.yaml"
     "llama3.1_70b_maxtext_pretraining" = "llama3-70b-bf16.yaml"
+    "mixtral8_70b_nemo_pretraining"    = "mixtral8-7b-bf16.yaml"
     "gke-nccl"                         = ""
   }[var.recipe]
 
@@ -10,6 +11,7 @@ locals {
     "llama3.1_7b_nemo_pretraining"     = "nemo-training"
     "llama3.1_70b_nemo_pretraining"    = "nemo-training"
     "llama3.1_70b_maxtext_pretraining" = "maxtext-training"
+    "mixtral8_70b_nemo_pretraining"    = "nemo-training"
     "gke-nccl"                         = ""
   }[var.recipe]
 }
--- applications/hcc/node_count.tf
+++ applications/hcc/node_count.tf
@@ -3,6 +3,7 @@ locals {
     "llama3.1_7b_nemo_pretraining"     = var.node_count_llama_3_7b
     "llama3.1_70b_nemo_pretraining"    = var.node_count_llama_3_70b
     "llama3.1_70b_maxtext_pretraining" = var.node_count_llama_3_70b
+    "mixtral8_70b_nemo_pretraining"    = var.node_count_llama_3_70b
     "gke-nccl"                         = var.node_count_gke_nccl
     "gke"                              = var.node_count_gke
   }[local.recipe]
