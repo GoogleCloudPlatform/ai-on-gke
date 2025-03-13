a96406c0d4ed8a286c28c94a64a398d10c3efe20
Srikanth Yaganti <sriyaganti@google.com>
2025-03-14 00:39:05 +0530

    Update charts/tpu-dra-driver/Chart.yaml
    Co-authored-by: Sergey Kanzhelev <S.Kanzhelev@live.com>

--- charts/tpu-dra-driver/install-tpu-dra-driver.sh
+++ charts/tpu-dra-driver/install-tpu-dra-driver.sh
@@ -8,8 +8,7 @@ set -o pipefail
 : ${DRIVER_NAME:=tpu-dra-driver}
 
 # The registry, image and tag for the example driver
-# Please update DRIVER_IMAGE_REGISTRY
-: ${DRIVER_IMAGE_REGISTRY:="gcr.io/gke-release-staging"}
+: ${DRIVER_IMAGE_REGISTRY:="gcr.io/gke-release"}
 : ${DRIVER_IMAGE_NAME:="${DRIVER_NAME}"}
 : ${DRIVER_IMAGE_TAG:="master.0"}
 : ${DRIVER_IMAGE_PLATFORM:="ubuntu22.04"}
@@ -20,7 +19,7 @@ set -o pipefail
 helm upgrade -i --create-namespace --namespace tpu-dra-driver tpu-dra-driver ${CURRENT_DIR} \
   --set image.repository=${DRIVER_IMAGE_REGISTRY}/${DRIVER_IMAGE_NAME} \
   --set image.tag=${DRIVER_IMAGE_TAG} \
-  --set image.pullPolicy=Always \
+  --set image.pullPolicy=IfNotPresent \
   --set cdi.enabled=true \
   --set cdi.default=true \
   --set controller.priorityClassName="" \
