adfde1c42b3650019a8c8253dfd93415f294990e
Srikanth Yaganti <sriyaganti@google.com>
2025-03-14 00:39:05 +0530

    Update tpu-dra-driver helm charts and add nvidia-dra-driver-gpu helm

--- charts/tpu-dra-driver/README.md
+++ charts/tpu-dra-driver/README.md
@@ -1,8 +1,12 @@
-# TPU DRA DRIVER
+# TPU DRA driver
 
-This helm chart is for running TPU DRA Driver Private Preview version on GKE
+This helm chart is for running TPU DRA driver on GKE. The driver is in Private Preview stage now. 
 
 ## Overview
+
+TPU DRA driver is only supported on GKE cluster version 1.32+
+Make sure to disable the default tpu-device-plugin on the nodes. This can be done by add node label
+`gke-no-default-tpu-device-plugin=true` and `gke-no-default-tpu-dra-plugin=true` when creating nodepool
 
 Run `./install-tpu-dra-driver.sh` to install tpu-dra-driver on your GKE Cluster
 nodes with TPU resources
--- charts/tpu-dra-driver/install-tpu-dra-driver.sh
+++ charts/tpu-dra-driver/install-tpu-dra-driver.sh
@@ -8,8 +8,7 @@ set -o pipefail
 : ${DRIVER_NAME:=tpu-dra-driver}
 
 # The registry, image and tag for the example driver
-# Please update DRIVER_IMAGE_REGISTRY
-: ${DRIVER_IMAGE_REGISTRY:="gcr.io/gke-release-staging"}
+: ${DRIVER_IMAGE_REGISTRY:="gcr.io/gke-release"}
 : ${DRIVER_IMAGE_NAME:="${DRIVER_NAME}"}
 : ${DRIVER_IMAGE_TAG:="master.0"}
 : ${DRIVER_IMAGE_PLATFORM:="ubuntu22.04"}
@@ -20,9 +19,12 @@ set -o pipefail
 helm upgrade -i --create-namespace --namespace tpu-dra-driver tpu-dra-driver ${CURRENT_DIR} \
   --set image.repository=${DRIVER_IMAGE_REGISTRY}/${DRIVER_IMAGE_NAME} \
   --set image.tag=${DRIVER_IMAGE_TAG} \
-  --set image.pullPolicy=Always \
+  --set image.pullPolicy=IfNotPresent \
   --set cdi.enabled=true \
   --set cdi.default=true \
   --set controller.priorityClassName="" \
   --set kubeletPlugin.priorityClassName="" \
   --set deviceClasses="{tpu}" \
+  --set kubeletPlugin.tolerations[0].key=google.com/tpu \
+  --set kubeletPlugin.tolerations[0].operator=Exists \
+  --set kubeletPlugin.tolerations[0].effect=NoSchedule
